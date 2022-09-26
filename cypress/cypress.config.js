e5f70e09e0032c762a625dbfd970ee8be2a1cbfd
claire nollet <clairenlet@gmail.com>
2022-09-26 16:54:03 +0200

    chore(projectInit): :see_no_evil: Add env in gitignore

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,16 +1,29 @@
 const { defineConfig } = require('cypress')
 
+const clientHost = process.env.CLIENT_HOST || 'localhost'
+const clientPort = process.env.CLIENT_PORT || 8080
+
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://dso-client:80',
+    baseUrl: `http://${clientHost}:${clientPort}`,
+    fixturesFolder: 'e2e/fixtures',
     specPattern: 'e2e/specs/**/*.{cy,e2e}.js',
     supportFile: 'e2e/support/index.js',
     video: false,
+    screenshotsFolder: 'e2e/screenshots',
+    numTestsKeptInMemory: 1,
   },
   components: {
-    baseUrl: 'http://dso-client:80',
     specPattern: 'components/specs/**/*.{cy,ct}.js',
     supportFile: 'components/support/index.js',
     video: false,
+    screenshotsFolder: 'components/screenshots',
+    numTestsKeptInMemory: 1,
+    devServer: {
+      framework: 'vue',
+      bundler: 'vite',
+      host: '127.0.0.1',
+      port: '8080',
+    },
   },
 })
