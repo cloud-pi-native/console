d18aa2072047cafd571934922d1386b8662d44a6
claire nollet <clairenlet@gmail.com>
2022-09-26 13:11:57 +0200

    test(projectInit): :white_check_mark: Add cypress config for components

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -3,8 +3,14 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://dso-client:80',
-    specPattern: '**/*.{cy,e2e}.js',
-    supportFile: 'support/index.js',
+    specPattern: 'e2e/specs/**/*.{cy,e2e}.js',
+    supportFile: 'e2e/support/index.js',
+    video: false,
+  },
+  components: {
+    baseUrl: 'http://dso-client:80',
+    specPattern: 'components/specs/**/*.{cy,ct}.js',
+    supportFile: 'components/support/index.js',
     video: false,
   },
 })
