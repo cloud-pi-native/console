c93de85d1b2e5ae604f3656ebca301c65f9a7563
claire nollet <clairenlet@gmail.com>
2022-10-01 12:17:23 +0200

    feat: :construction: wip business rules orderProject

--- client/src/views/OrderProject.vue
+++ client/src/views/OrderProject.vue
@@ -1,16 +1,40 @@
 <script setup>
-import { ref } from 'vue'
-defineProps({
+import { computed, ref } from 'vue'
 
+const project = ref({
+  email: undefined,
+  orgName: undefined,
+  projectName: undefined,
+  repo: [],
 })
 
-const projects = ref([{}])
+const isRepoValid = computed(() => {
+  // TODO : itérer pour chaque repo
+  // forEach(project.value.repo)
+  // gitName &&
+  // if gitSourceName, managerName &&
+  // if isGitSourcePrivate, gitToken
+  return true
+})
 
-const isGitSourceFilled = ref(false)
+const isFormValid = computed(() => {
+  return project.value.email &&
+      project.value.orgName &&
+      project.value.projectName &&
+      isRepoValid
+})
 
-// const addProject = () => {
-//   projects.value (pusher objet)
-// }
+const addRepo = () => {
+  project.value.repo.push({})
+}
+
+const updateRepo = (index, key, value) => {
+  project.value.repo[index][key] = value
+}
+
+const orderProject = () => {
+  console.log(project.value)
+}
 
 </script>
 
@@ -19,62 +43,94 @@ const isGitSourceFilled = ref(false)
     legend="Commande d'un espace projet"
   >
     <DsfrInput
-      v-model="email"
+      v-model="project.email"
       type="email"
-      required
-      label="Votre e-mail"
+      required="required"
+      autocomplete="email"
+      label="E-mail professionnel"
       label-visible
-      placeholder="eric.tiquet@interieur.gouv.fr"
+      placeholder="prenom.nom@interieur.gouv.fr"
+      class="fr-mb-2w"
     />
     <DsfrInput
-      v-model="orgName"
+      v-model="project.orgName"
       type="text"
-      required
+      required="required"
+      autocomplete="organization"
       label="Nom de l'organisation"
       label-visible
       placeholder="SDIT"
+      class="fr-mb-2w"
     />
     <DsfrInput
-      v-model="projectName"
+      v-model="project.projectName"
       type="text"
-      required
+      required="required"
       label="Nom du projet"
       label-visible
       placeholder="MonProjet"
+      class="fr-mb-2w"
     />
   </DsfrFieldset>
-  <div class="fr-mt-4w" />
-  <DsfrFieldset
-    v-for="(project, index) in projects"
-    :key="`project-${index}`"
-    legend="Dépôt d'un projet Git"
+  <DsfrButton
+    label="Déposer un projet git"
+    secondary
+    icon="ri-upload-cloud-line"
+    @click="addRepo()"
+  />
+  <DsfrButton
+    label="Commander mon espace projet"
+    primary
+    :disabled="!isFormValid"
+    icon="ri-send-plane-line"
+    class="fr-ml-2w"
+    @click="orderProject()"
+  />
+  <div
+    v-if="project.repo.length"
+    class="fr-mt-4w"
   >
-    <DsfrInput
-      v-model="inputValue"
-      type="text"
-      label="Nom du dépôt git"
-      required
-      label-visible
-    />
-    <DsfrInput
-      v-model="inputValue"
-      type="text"
-      label="Nom du dépôt git source"
-      label-visible
-    />
-    <DsfrInput
-      v-model="inputValue"
-      type="text"
-      label="Nom du responsable"
-      :required="isGitSourceFilled"
-      hint="Nom de l'organisation ou de l'utilisateur propriétaire du dépôt git source"
-      label-visible
-    />
-    <DsfrInput
-      v-model="inputValue"
-      type="text"
-      label="Token d'accès git"
-      label-visible
-    />
-  </DsfrFieldset>
+    <DsfrFieldset
+      v-for="(repo, index) in project.repo"
+      :key="`project-${index}`"
+      :legend="project.repo.length === 1 ? 'Dépôt git' : `Dépôt git ${index + 1}`"
+    >
+      <DsfrInput
+        type="text"
+        required="required"
+        label="Nom du dépôt git"
+        label-visible
+        placeholder="dso-console"
+        class="fr-mb-2w"
+        @update:model-value="updateRepo(index, 'gitName', $event)"
+      />
+      <DsfrInput
+        type="text"
+        label="Nom du dépôt git source"
+        label-visible
+        placeholder="dso-console"
+        class="fr-mb-2w"
+        @update:model-value="updateRepo(index, 'gitSourceName', $event)"
+      />
+      <DsfrInput
+        type="text"
+        :required="repo.gitSourceName ? 'required' : false"
+        autocomplete="name"
+        label="Nom du responsable"
+        label-visible
+        hint="Nom de l'organisation ou de l'utilisateur propriétaire du dépôt git source"
+        class="fr-mb-2w"
+        @update:model-value="updateRepo(index, 'managerName', $event)"
+      />
+      <DsfrInput
+        v-model="gitToken"
+        type="text"
+        label="Token d'accès git"
+        label-visible
+        placeholder="ghp_exemple"
+        class="fr-mb-2w"
+        @update:model-value="updateRepo(index, 'gitToken', $event)"
+      />
+    </DsfrFieldset>
+  </div>
 </template>
