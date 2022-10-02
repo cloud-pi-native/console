c9e3806b118c2ff878cf5d8b027740fa138dce6f
claire nollet <clairenlet@gmail.com>
2022-10-02 02:13:20 +0200

    feat: :sparkles: Add DsoService content

--- client/src/views/DsoServices.vue
+++ client/src/views/DsoServices.vue
@@ -1,7 +1,19 @@
 <script setup>
+import { ref } from 'vue'
+
+// TODO : récupérer les services dynamiquement
+const tiles = ref([{
+  title: 'Gitlab',
+  imgSrc: 'logoGitlab',
+  description: 'GitLab est un service d\'hébergement de code source et de pipeline CI/CD',
+  to: 'https://about.gitlab.com/',
+}])
 
 </script>
 
 <template>
-  <p>Services</p>
+  <DsfrTiles
+    :tiles="tiles"
+    class="fr-mt-2v"
+  />
 </template>
