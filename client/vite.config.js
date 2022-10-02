8ae814b1a88c1a5fb98b2188391ebda3633a62d2
claire nollet <clairenlet@gmail.com>
2022-10-02 23:33:37 +0200

    feat: :sparkles: Add windicss to project

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
+import WindiCSS from 'vite-plugin-windicss'
 import vue from '@vitejs/plugin-vue'
 import { keycloakHost, keycloakPort } from './src/utils/oidc/sso-config.js'
 
@@ -12,6 +13,7 @@ export default defineConfig(mode => ({
   },
   plugins: [
     vue(),
+    WindiCSS(),
   ],
   base: process.env.BASE_URL || '/',
   resolve: {
