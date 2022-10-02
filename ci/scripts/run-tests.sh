5a86e7f63a1700d78ec0aff945aa8d753807595e
this-is-tobi <thibault_colin@hotmail.fr>
2022-10-02 21:46:58 +0200

    ci: :construction_worker: Add tool scripts

--- ci/scripts/run-tests.sh
+++ ci/scripts/run-tests.sh
@@ -17,85 +17,85 @@ NPM_VERSION="$(npm --version)"
 DOCKER_VERSION="$(docker --version)"
 DOCKER_COMPOSE_VERSION="$(docker compose version)"
 
-# Default arguments
-RUN_UNIT_TESTS=false
-RUN_E2E_TESTS=false
+# Default
+ENV_FILE="$PROJECT_DIR/cypress/env/.env"
 
 # Declare script helper
 TEXT_HELPER="\nThis script aims to run application tests.
-
 Following flags are available:
 
-  -e, --e2e     Run e2e tests.
+  -e    Run e2e tests
 
-  -u, --unit    Run unit tests.
+  -u    Run unit tests for the given directory
+        This argument can be call multiple times if needed
 
-  -h, --help    Print script help.\n\n"
+  -h    Print script help\n\n"
 
 print_help() {
   printf "$TEXT_HELPER"
 }
 
 # Parse options
-while getopts :heu-: flag
+while getopts heu: flag
 do
   case "${flag}" in
-    -)
-      case "${OPTARG}" in
-        e2e)
-          RUN_E2E_TESTS=true;;
-        unit)
-          RUN_UNIT_TESTS=true;;
-        help | *)
-          print_help
-          exit 0;;
-      esac;;
     e)
       RUN_E2E_TESTS=true;;
     u)
-      RUN_UNIT_TESTS=true;;
+      UNIT_TESTS_DIRS+=(${OPTARG});;
     h | *)
       print_help
       exit 0;;
   esac
 done
 
-if [ "$RUN_E2E_TESTS" = false ] && [ "$RUN_UNIT_TESTS" = false ]; then
+
+# Script condition
+if [ ! "$RUN_E2E_TESTS" ] && [ ! "$UNIT_TESTS_DIRS" ]; then
   echo "\nArgument(s) missing, you don't specify any kind of test to run."
   print_help
   exit 0
 fi
 
-# Copy env example files
-ENV_FILE=cypress/env/.env
-if [ ! -f "$PROJECT_DIR/$ENV_FILE" ]; then
+if [ ! -f "$ENV_FILE" ]; then
   printf "\n${red}Optional.${no_color} Trying to copy file '.env-example' to '.env' because it's missing\n"
 
-  if [ -f "$PROJECT_DIR/$ENV_FILE-example" ]; then
-    cp $PROJECT_DIR/$ENV_FILE-example $PROJECT_DIR/$ENV_FILE
+  if [ -f "$ENV_FILE-example" ]; then
+    cp $ENV_FILE-example $ENV_FILE
     printf "\n${red}Optional.${no_color} Successful copying\n"
   else
-    printf "\n${red}Optional.${no_color} Error while trying to copy, '$PROJECT_DIR/$ENV_FILE-example' is missing too\n"
+    printf "\n${red}Optional.${no_color} Error while trying to copy, '$ENV_FILE-example' is missing too\n"
     exit 1
   fi
 fi
 
-# Run tests
+
+# Settings
 printf "\nScript settings:
   -> node version: $NODE_VERSION
   -> npm version: $NPM_VERSION
   -> docker version: $DOCKER_VERSION
   -> docker-compose version: $DOCKER_COMPOSE_VERSION\n"
 
-if [ "$RUN_UNIT_TESTS" = true ]; then
-  printf "\n${red}${i}.${no_color} Launch unit tests\n"
-  i=$(($i + 1))
 
-  cd $PROJECT_DIR
-  npm run test:client
+# Run unit tests
+if [ "$UNIT_TESTS_DIRS" ]; then
+  for d in "${UNIT_TESTS_DIRS[@]}"; do
+    if [ ! -d "$d" ]; then
+      printf "\nThe given directory does not exist, skipping unit tests for : ${red}${d}${no_color}.\n"
+      continue
+    fi
+
+    printf "\n${red}${i}.${no_color} Launch unit tests for directory: ${red}$(readlink -f $d)${no_color}\n"
+    i=$(($i + 1))
+
+    cd "$(readlink -f $d)"
+    npm run test:unit
+  done
 fi
 
-if [ "$RUN_E2E_TESTS" = true ]; then
+# Run e2e tests
+if [ "$RUN_E2E_TESTS" ]; then
   if [ ! -x "$(command -v docker)" ]; then
     echo "\nThis script uses docker, and it isn't running - please start docker and try again!\n"
     exit 1
@@ -108,6 +108,22 @@ if [ "$RUN_E2E_TESTS" = true ]; then
   printf "\n${red}${i}.${no_color} Launch e2e tests\n"
   i=$(($i + 1))
 
-  cd $PROJECT_DIR
-  npm run test:e2e
+  cd "$PROJECT_DIR"
+  export DOCKER_TAG=latest
+
+  docker compose \
+    --file "$PROJECT_DIR/docker/docker-compose.prod.yml" \
+    --file "$PROJECT_DIR/docker/docker-compose.e2e.yml" up \
+      --exit-code-from cypress \
+      --attach cypress \
+      --remove-orphans \
+      --pull always
+  
+  printf "\n${red}${i}.${no_color} Remove stopped containers\n"
+  i=$(($i + 1))
+
+  docker compose \
+    --file "$PROJECT_DIR/docker/docker-compose.prod.yml" \
+    --file "$PROJECT_DIR/docker/docker-compose.e2e.yml" down \
+      --volumes
 fi
