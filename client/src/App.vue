61670d816aa3b70aa164e3d8531b12b3f835fcc6
claire nollet <clairenlet@gmail.com>
2022-10-03 21:49:32 +0200

    feat: :lipstick: Resize sidemenu and content

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,11 +8,11 @@ import SideMenu from './components/SideMenu.vue'
     service-title="Portail Cloud PI Native"
     :logo-text="['Ministère', 'de l’intérieur']"
   />
-  <div class="fr-container  fr-grid-row">
-    <div class="fr-col-12 fr-col-md-4">
+  <div class="fr-container fr-grid-row">
+    <div class="fr-col-12 fr-col-md-3">
       <SideMenu />
     </div>
-    <div class="fr-col-12 fr-col-md-8 fr-py-12v">
+    <div class="fr-col-12 fr-col-md-9 fr-py-12v">
       <router-view />
     </div>
   </div>
