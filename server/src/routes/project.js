a2d038817683941a25a905d2dd1dc5d0bc7af8d4
claire nollet <clairenlet@gmail.com>
2022-10-11 11:16:32 +0200

    feat: :construction: Add routes for projects

--- server/src/routes/project.js
+++ server/src/routes/project.js
@@ -3,16 +3,12 @@ import {
   getProjectController,
   getProjectsController,
   createProjectController,
-  updateProjectController,
-  deleteProjectController,
-} from '../controllers/project-controllers.js'
+} from '../controllers/project.js'
 
 const router = new express.Router()
 
 router.post('/', createProjectController)
 router.get('/', getProjectController)
 router.get('/:id', getProjectsController)
-router.put('/:id', updateProjectController)
-router.delete('/:id', deleteProjectController)
 
 export default router
