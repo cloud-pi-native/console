d41138606432df576abf0f5e8d05b1a8f2bcc5ad
claire nollet <clairenlet@gmail.com>
2022-10-12 13:13:28 +0200

    feat: :construction: wip add node-postgres

--- server/src/connect.js
+++ server/src/connect.js
@@ -1,1 +1,22 @@
 // TODO : open et close connexion to db
+
+import Pool from 'pg'
+
+let pool
+
+export const getConnection = async () => {
+  pool = new Pool()
+}
+
+export const query = async (text, params, callback) => {
+  console.log('start query')
+  const res = await pool.query(text, params, callback)
+  console.log('end query, res: ', res)
+  return res
+}
+
+export const closeConnections = async () => {
+  console.log('closing')
+  await pool.end()
+  console.log('closed')
+}
--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 import { createServer } from 'http'
 import fs from 'fs'
 import app from './app.js'
-// import { getConnection, closeConnections } from './connect.js'
+import { getConnection, closeConnections } from './connect.js'
 import { techLogger } from './utils/logger.js'
 import { isDev, isTest, isProd } from './utils/env.js'
 
@@ -12,7 +12,7 @@ handleExit()
 
 export async function startServer () {
   try {
-    // await getConnection()
+    await getConnection()
   } catch (error) {
     techLogger.error(error.message)
     throw error
@@ -20,32 +20,32 @@ export async function startServer () {
 
   techLogger.info('server connected to postgres, reading init-db.js')
 
-  try {
-    // const { initDb } = await import('../dev-setup/init-db.js')
-    const initDb = false
-    if (initDb) {
-      techLogger.info('Starting init DB...')
-      await initDb()
-      techLogger.info('DB successfully init')
-    }
-    if (isProd && initDb) {
-      techLogger.info('Cleaning up init file...')
-      await new Promise((resolve, reject) => {
-        fs.unlink('./dev-setup/init-db.js', (err) => {
-          if (err) return reject(err)
-          techLogger.info('Successfully deleted ../dev-setup/init-db.js')
-          resolve()
-        })
-      })
-    }
-  } catch (error) {
-    if (error.code === 'ERR_MODULE_NOT_FOUND' || error.message.startsWith('Cannot find module')) {
-      techLogger.info('No initDb file, skipping')
-    } else {
-      techLogger.warn(error.message)
-      throw error
-    }
-  }
+  // try {
+  //   // const { initDb } = await import('../dev-setup/init-db.js')
+  //   const initDb = false
+  //   if (initDb) {
+  //     techLogger.info('Starting init DB...')
+  //     await initDb()
+  //     techLogger.info('DB successfully init')
+  //   }
+  //   if (isProd && initDb) {
+  //     techLogger.info('Cleaning up init file...')
+  //     await new Promise((resolve, reject) => {
+  //       fs.unlink('./dev-setup/init-db.js', (err) => {
+  //         if (err) return reject(err)
+  //         techLogger.info('Successfully deleted ../dev-setup/init-db.js')
+  //         resolve()
+  //       })
+  //     })
+  //   }
+  // } catch (error) {
+  //   if (error.code === 'ERR_MODULE_NOT_FOUND' || error.message.startsWith('Cannot find module')) {
+  //     techLogger.info('No initDb file, skipping')
+  //   } else {
+  //     techLogger.warn(error.message)
+  //     throw error
+  //   }
+  // }
 
   createServer(app).listen(port, '0.0.0.0')
   techLogger.info(`server running at http://localhost:${port}`)
@@ -64,7 +64,7 @@ export async function exitGracefuly (error) {
     techLogger.error(error)
   }
   techLogger.info('Closing connections...')
-  // await closeConnections()
+  await closeConnections()
   techLogger.info('Exiting...')
   process.exit(error instanceof Error ? 1 : 0)
 }
