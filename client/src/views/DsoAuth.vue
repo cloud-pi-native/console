c28944db5fd3453d9107ab7cf093326a87b1f80d
claire nollet <clairenlet@gmail.com>
2022-10-12 12:35:38 +0200

    fix: :bug: Fix endless redirection

--- client/src/views/DsoAuth.vue
+++ client/src/views/DsoAuth.vue
@@ -1,12 +1,3 @@
-<script setup>
-import { initKeycloak } from '@/utils/keycloak/init-sso.js'
-import { onMounted } from 'vue'
-
-onMounted(async () => {
-  await initKeycloak()
-})
-</script>
-
 <template>
   <h1>Authentification</h1>
   <h2>...redirection en cours</h2>
