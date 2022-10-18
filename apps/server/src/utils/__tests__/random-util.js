7350ae5c092d18bae1d031e95d79bc03f36887f4
this-is-tobi <thibault_colin@hotmail.fr>
2022-10-18 15:47:48 +0200

    chore: :art: Improve utils

--- apps/server/src/utils/__tests__/random-util.js
+++ apps/server/src/utils/__tests__/random-util.js
@@ -25,7 +25,7 @@ export const getRandomProjectRepo = (length = 1) => {
     projectRepo.gitName = faker.lorem.word()
     projectRepo.gitSourceName = faker.lorem.word()
     projectRepo.managerName = faker.name.fullName()
-    projectRepo.isPrivate = faker.datatype.boolean
+    projectRepo.isPrivate = faker.datatype.boolean()
     if (projectRepo.isPrivate) projectRepo.gitToken = faker.git.shortSha()
 
     repo.push(projectRepo)
