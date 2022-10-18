8791c50df1d46a67531bd37f06b42ff2f7739d3b
this-is-tobi <thibault_colin@hotmail.fr>
2022-10-18 03:55:33 +0200

    ci: :construction_worker: Update volumes & docker compose schema version

--- db/postgres/scripts/010-init-db.sql
+++ db/postgres/scripts/010-init-db.sql
@@ -1,5 +1,7 @@
 -- Creation of projects table
-CREATE TABLE IF NOT EXISTS projects (
-  id       INT PRIMARY KEY, 
-  project  JSONB NOT NULL
+CREATE TABLE IF NOT EXISTS public.projects (
+  id    SERIAL  NOT NULL PRIMARY KEY, 
+  data  JSONB   NOT NULL
 );
+
+CREATE UNIQUE INDEX project_id ON public.projects ( (data ->> 'id') );
