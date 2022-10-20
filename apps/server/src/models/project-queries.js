2ebf121e51b84ff4c033665212a8794f2d21457d
this-is-tobi <thibault_colin@hotmail.fr>
2022-10-20 13:03:11 +0200

    build: :heavy_plus_sign: Add sequelize orm for api

--- apps/server/src/models/project-queries.js
+++ apps/server/src/models/project-queries.js
@@ -1,21 +1,24 @@
-import { query } from '../connect.js'
-import { nanoid } from 'nanoid'
-import { allServices } from '../utils/project.js'
+import { Op } from 'sequelize'
+import Project from './project.js'
 
 export const createProject = async (data) => {
-  data.id = nanoid()
-  data.services = allServices
-  data = JSON.stringify(data)
-  const res = await query(`INSERT INTO public.projects(data) VALUES ('${data}');`)
-  return res.rows[0]
+  const res = await Project().create({ data })
+  return res
 }
 
 export const getProjectById = async (id) => {
-  const res = await query(`SELECT data FROM projects WHERE (data ->> 'id') = '${id}' LIMIT 1;`)
-  return res.rows[0]
+  const res = await Project().findOne({
+    where: {
+      'data.id': {
+        [Op.eq]: id,
+      },
+    },
+    attributes: ['data'],
+  })
+  return res
 }
 
 export const getProjects = async () => {
-  const res = await query('SELECT data FROM projects')
-  return res.rows
+  const res = await Project().findAll({ attributes: ['data'] })
+  return res
 }
