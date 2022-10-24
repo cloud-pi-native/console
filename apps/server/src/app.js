b736991208b9a5fc0880fba294fa96dc073f4c8a
baudointran <baudointran@users.noreply.github.com>
2022-10-24 20:29:57 +0200

    fix: run with delay 10 second to wait keycloak initialize

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -1,22 +1,23 @@
 import fastify from 'fastify'
 import helmet from '@fastify/helmet'
-// import keycloak from 'fastify-keycloak-adapter'
+import keycloak from 'fastify-keycloak-adapter'
 
 import routes from './routes/index.js'
 import { loggerConf } from './utils/logger.js'
 
 export const apiPrefix = '/api/v1'
 
-// const opts = {
-//   appOrigin: 'http://localhost:8080',
-//   keycloakSubdomain: 'keycloak:8080/realms/TEST',
-//   clientId: 'TEST',
-//   clientSecret: 'TEST',
-// }
+const opts = {
+  appOrigin: 'http://keycloak:8080',
+  keycloakSubdomain: 'keycloak:8080/realms/TEST',
+  clientId: 'TEST',
+  clientSecret: 'TEST',
+  retries: 10,
+}
 
 const app = fastify({ logger: loggerConf[process.env.NODE_ENV] ?? true })
   .register(helmet)
-  // .register(keycloak, opts)
+  .register(keycloak, opts)
   .register(routes, { prefix: apiPrefix })
 
 export default app
