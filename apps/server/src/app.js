0269aa29479f6f3dc4275ffc10a104de4990ec4d
Claire Nollet <clairenlet@gmail.com>
2022-10-24 18:47:47 +0200

    feat: :construction: (wip) trying to connect to keycloak with server

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -1,12 +1,22 @@
 import fastify from 'fastify'
 import helmet from '@fastify/helmet'
+// import keycloak from 'fastify-keycloak-adapter'
+
 import routes from './routes/index.js'
 import { loggerConf } from './utils/logger.js'
 
 export const apiPrefix = '/api/v1'
 
+// const opts = {
+//   appOrigin: 'http://localhost:8080',
+//   keycloakSubdomain: 'keycloak:8080/realms/TEST',
+//   clientId: 'TEST',
+//   clientSecret: 'TEST',
+// }
+
 const app = fastify({ logger: loggerConf[process.env.NODE_ENV] ?? true })
   .register(helmet)
+  // .register(keycloak, opts)
   .register(routes, { prefix: apiPrefix })
 
 export default app
