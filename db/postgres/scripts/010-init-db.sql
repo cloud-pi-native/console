7a6987dd78e792580606e37195924acedee69973
Claire Nollet <clairenlet@gmail.com>
2022-10-24 11:31:42 +0200

    fix: :bug: Fix init db error

--- db/postgres/scripts/010-init-db.sql
+++ db/postgres/scripts/010-init-db.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS public.Projects (
   id          SERIAL  NOT NULL PRIMARY KEY, 
   data        JSONB   NOT NULL,
   createdAt   DATE,
-  updatedAt   DATE,
+  updatedAt   DATE
 );
 
 CREATE UNIQUE INDEX project_id ON public.Projects ( (data ->> 'id') );
