48949df562179493e2b524b541129ff520913d86
Claire Nollet <clairenlet@gmail.com>
2022-10-26 19:26:05 +0200

    feat: :card_file_box: Add user id in project queries

--- apps/server/src/models/project.js
+++ apps/server/src/models/project.js
@@ -2,7 +2,7 @@ import { DataTypes } from 'sequelize'
 import { sequelize } from '../connect.js'
 
 let Project
-export const getProject = () => Project ?? (Project = sequelize.define('Project', {
+export const getProjectModel = () => Project ?? (Project = sequelize.define('Project', {
   id: {
     type: DataTypes.INTEGER,
     allowNull: false,
