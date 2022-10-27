cfb716a168f83bdeeab8f4f8b18540f5367f33de
this-is-tobi <thibault_colin@hotmail.fr>
2022-10-27 23:44:29 +0200

    test: :white_check_mark: Fix unit tests for server

--- apps/server/src/app.spec.js
+++ apps/server/src/app.spec.js
@@ -1,6 +1,10 @@
+import { vi } from 'vitest'
+import fp from 'fastify-plugin'
 import app, { apiPrefix } from './app.js'
 
 const version = process.env.npm_package_version
+
+vi.mock('fastify-keycloak-adapter', () => ({ default: fp(async () => vi.fn()) }))
 
 describe('app', () => {
   it('should respond with the version', async () => {
