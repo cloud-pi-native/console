d23971b08a13460cc77607661baca37ce0a9da1f
Claire Nollet <clairenlet@gmail.com>
2022-11-02 15:34:11 +0100

    test: :white_check_mark: Add unit test for schemaValidator func

--- apps/client/src/utils/func.spec.js
+++ apps/client/src/utils/func.spec.js
@@ -1,7 +1,32 @@
-import { identity } from '@/utils/func.js'
+import { identity, schemaValidator } from '@/utils/func.js'
+import { repoSchema } from 'shared/src/projects/schema.js'
+import { it } from 'vitest'
 
 describe('func', () => {
   it('Should return identity', () => {
     expect(identity('Test')).toStrictEqual('Test')
+  })
+
+  it('Should validate undefined schema', () => {
+    expect(schemaValidator(repoSchema, undefined)).toStrictEqual({})
+  })
+
+  it('Should validate correct schema', () => {
+    expect(schemaValidator(repoSchema, {
+      gitName: 'candilib',
+      gitSourceName: 'https://candilib.com',
+      gitToken: 'eddddsqsq',
+      isPrivate: true,
+      userName: 'clairenlet',
+    })).toStrictEqual({})
+  })
+
+  it('Should not validate schema and send specific error', () => {
+    expect(schemaValidator(repoSchema, {
+      gitName: 'candilib',
+      gitSourceName: 'https://candilib.com',
+      isPrivate: true,
+      userName: 'clairenlet',
+    })).toStrictEqual({ gitToken: '"gitToken" is required' })
   })
 })
