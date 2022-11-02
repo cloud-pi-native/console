81e3671b7bfc11f4f7052d1a96594ffc65f3e372
this-is-tobi <thibault_colin@hotmail.fr>
2022-11-02 06:12:22 +0100

    test: :package: Update shared & cypress packages

---
+++ packages/shared/dev-setup/projects.js
@@ -0,0 +1,58 @@
+export const projects = [
+  {
+    id: '9FG4CeGkMavI5CtAh_3Ss',
+    repo: [
+      {
+        gitName: 'candilib',
+        gitSourceName: 'https://github.com/dnum-mi/candilib',
+        isPrivate: true,
+        userName: 'this-is-a-test',
+        gitToken: 'this-is-a-token',
+      },
+    ],
+    owner: {
+      id: 'cb8e5b4b-7b7b-40f5-935f-594f48ae6565',
+      firstName: 'test',
+      lastName: 'TEST',
+      email: 'test@test.com',
+    },
+    orgName: 'ministere-interieur',
+    services: [
+      'argocd',
+      'gitlab',
+      'nexus',
+      'quay',
+      'sonarqube',
+      'vault',
+    ],
+    projectName: 'candilib',
+  },
+  {
+    id: '9FG4CeGkMavI5CtAh_3St',
+    repo: [
+      {
+        gitName: 'psij',
+        gitSourceName: 'https://github.com/dnum-mi/psij',
+        isPrivate: true,
+        userName: 'this-is-a-test',
+        gitToken: 'this-is-a-token',
+      },
+    ],
+    owner: {
+      id: 'cb8e5b4b-7b7b-40f5-935f-594f48ae6565',
+      firstName: 'test',
+      lastName: 'TEST',
+      email: 'test@test.com',
+    },
+    orgName: 'ministere-interieur',
+    services: [
+      'argocd',
+      'gitlab',
+      'nexus',
+      'quay',
+      'sonarqube',
+      'vault',
+    ],
+    projectName: 'psij',
+  },
+]
