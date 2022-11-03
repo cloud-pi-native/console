85d5a1c98f9374ba9c7fb9b4f179bb8f40a6b0f3
Claire Nollet <clairenlet@gmail.com>
2022-11-03 18:32:05 +0100

    feat: :sparkles: Add user to project

--- apps/client/src/views/projects/DsoTeam.vue
+++ apps/client/src/views/projects/DsoTeam.vue
@@ -1,15 +1,20 @@
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
 import DsoSelectedProject from './DsoSelectedProject.vue'
 import { useProjectStore } from '@/stores/project.js'
 import { DsfrTag } from '@gouvminint/vue-dsfr'
+import { userSchema } from 'shared/src/schemas/user.js'
+import { schemaValidator, isValid } from 'shared/src/utils/schemas.js'
 
 const projectStore = useProjectStore()
 
 const selectedProject = computed(() => projectStore.selectedProject)
 
+const newUserEmail = ref('')
+
+const newUser = ref({})
+
 const headers = [
-  'id',
   'E-mail',
   'Prénom',
   'Nom',
@@ -22,7 +27,7 @@ const rows = ref([])
 const setRows = () => {
   rows.value = []
 
-  rows.value.push([...Object.values(selectedProject.value.owner), {
+  rows.value.push([...Object.values(selectedProject.value.owner).filter(val => val !== selectedProject.value.owner.id), {
     component: DsfrTag,
     label: 'owner',
     class: 'fr-tag--dismiss',
@@ -32,7 +37,7 @@ const setRows = () => {
 
   if (selectedProject.value.users) {
     selectedProject.value.users.forEach(user => {
-      rows.value.push([...Object.values(user), {
+      rows.value.push([...Object.values(user).filter(val => val !== user.id), {
         component: DsfrTag,
         label: 'user',
         class: 'fr-tag--dismiss',
@@ -41,11 +46,28 @@ const setRows = () => {
       }])
     })
   }
+}
 
-  console.log({ rows: rows.value })
+const addUserToProject = () => {
+  // TODO : récupérer données keycloak de l'utilisateur via son e-mail ?
+  newUser.value.id = 'xxxxxx'
+  newUser.value.email = newUserEmail.value
+  newUser.value.firstName = newUserEmail.value.split('.')[0]
+  newUser.value.lastName = newUserEmail.value.split('.')[1].split('@')[0]
+
+  const keysToValidate = ['id', 'email', 'firstName', 'lastName']
+  const errorSchema = schemaValidator(userSchema, newUser.value, keysToValidate)
+
+  if (Object.keys(errorSchema).length || selectedProject.value.users.find(user => user.email === newUserEmail.value)) return
+  projectStore.addUserToProject(newUser.value)
 }
 
 onMounted(() => {
+  setRows()
+})
+
+// TODO : comprendre pq watch sur la computed ne fonctionne pas
+watch(projectStore.selectedProject, () => {
   setRows()
 })
 
@@ -63,10 +85,21 @@ onMounted(() => {
     :headers="headers"
     :rows="rows"
   />
+  <DsfrInput
+    v-model="newUserEmail"
+    hint="Adresse e-mail associée au compte keycloak de l'utilisateur"
+    type="text"
+    label="Ajouter un utilisateur via son adresse e-mail"
+    label-visible
+    placeholder="prenom.nom@interieur.gouv.fr"
+    :is-valid="!!newUserEmail && isValid(userSchema, newUser, 'email')"
+    :is-invalid="!!newUserEmail && !isValid(userSchema, newUser, 'email')"
+    class="fr-mb-2w"
+  />
   <DsfrButton
-    label="Ajouter un utilisateur"
+    label="Ajouter l'utilisateur"
     secondary
     icon="ri-user-add-line"
-    @click="onClick()"
+    @click="addUserToProject()"
   />
 </template>
