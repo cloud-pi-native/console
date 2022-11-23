210653b3cf07a3ffa66dafc98305f78a953a4780
Claire Nollet <clairenlet@gmail.com>
2022-11-23 20:08:28 +0100

    test: :test_tube: Try to fix in ci redirection test

--- apps/client/cypress/e2e/specs/redirection.e2e.js
+++ apps/client/cypress/e2e/specs/redirection.e2e.js
@@ -8,12 +8,18 @@ afterEach(() => {
 
 describe('Redirection', () => {
   it('Should redirect to original page on reload', () => {
+    cy.intercept('GET', '/api/v1/projects').as('getProjects')
     cy.kcLogin('test')
       .visit('/')
       .reload()
       .url().should('match', /\/#state=/)
       .goToProjects()
       .reload()
+      .wait('@getProjects').its('response').then(response => {
+        cy.log(response.body.length)
+          .get('[data-testid^="projectTile-"]')
+          .should('have.length', `${response.body.length}`)
+      })
       .url().should('match', /\/projects#state=/)
       .getByDataTestid(`projectTile-${candilib.projectName}`).click()
       .reload()
@@ -24,11 +30,11 @@ describe('Redirection', () => {
 
   it('Should redirect to login page if not logged in', () => {
     cy.visit(`/projects/${candilib.id}/dashboard`)
-      .url().should('match', /realms\/cloud-pi-native\/protocol\/openid-connect/)
+      .url().should('not.contain', `/projects/${candilib.id}/dashboard`)
       .get('input#username').type('test')
       .get('input#password').type('test')
       .get('input#kc-login').click()
-      .url().should('match', new RegExp(`/projects/${candilib.id}/dashboard#state=`))
+      .url().should('contain', `/projects/${candilib.id}/dashboard`)
       .getByDataTestid('currentProjectInfo')
       .should('contain', `Le projet courant est : ${candilib.projectName}`)
   })
