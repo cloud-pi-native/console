7495094c76735d00335f6425605d158209882abb
this-is-tobi <thibault_colin@hotmail.fr>
2022-11-24 02:21:03 +0100

    ci: :construction_worker: Add ansible api report in sonar

--- packages/tests-utils/src/random-project.js
+++ packages/tests-utils/src/random-project.js
@@ -5,13 +5,15 @@ import {
   getRandomProjectServices,
   getRandomRepo,
   getRandomUser,
+  getRandomEnvList,
 } from './random-utils.js'
 
-export const createRandomProject = (nbRepos = 3, nbUsers = 3) => ({
+export const createRandomProject = (nbRepos = 3, nbUsers = 3, nbEnv = 3) => ({
   owner: getRandomUser(),
   orgName: getRandomProjectOrgName(),
   projectName: getRandomProjectName(),
   services: getRandomProjectServices(),
   repos: repeatFn(nbRepos)(getRandomRepo),
   users: repeatFn(nbUsers)(getRandomUser),
+  envList: getRandomEnvList(nbEnv),
 })
