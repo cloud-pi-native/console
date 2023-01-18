2dcaf2ebf6ed0784bce95f44d6442d9cd1d1483d
Arnaud TARDIF <atardif@adlere.fr>
2023-01-18 02:21:44 +0100

    refactor: :construction: ajout de requetes sequelize

---
+++ apps/server/src/models/organization-queries.js
@@ -0,0 +1,24 @@
+import { getOrganizationModel } from './project.js'
+
+// SELECT
+export const getOrganizations = async () => {
+  const res = await getOrganizationModel().findAll()
+  return res
+}
+
+export const getOrganizationByName = async (name) => {
+  const res = await getOrganizationModel().findAll({
+    where: { name },
+  })
+  return res
+}
+
+// CREATE
+export const createOrganization = async (name) => {
+  const organizations = await getOrganizationByName(name)
+  if (!organizations.length) {
+    const res = await getOrganizationModel().create({ name })
+    return res
+  }
+  return organizations
+}
