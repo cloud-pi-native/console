8d42f7e46d2f3ed33b2b9c4da1c1afdb31eac758
Nonot <gitlab.com@aillo.li>
2023-01-20 01:03:55 +0100

    fix: :bug: fix front projects detection path cause uuid

--- apps/client/src/router/index.js
+++ apps/client/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useUserStore } from '@/stores/user.js'
 import { useProjectStore } from '@/stores/project.js'
-import { idInUrl } from '@/utils/regex.js'
 
 import DsoHome from '@/views/DsoHome.vue'
 import CreateProject from '@/views/CreateProject.vue'
@@ -111,13 +110,13 @@ router.beforeEach(async (to, _from, next) => {
  */
 router.beforeEach(async (to, _from, next) => {
   const projectStore = useProjectStore()
+  const projectsPath = '/projects/'
 
-  if (to.path.match('^/projects/') && projectStore.selectedProject === undefined) {
+  if (to.path.match(`^${projectsPath}.{36+}`) && projectStore.selectedProject === undefined) {
     await projectStore.getUserProjects()
 
-    const idStart = to.path.search(idInUrl)
-    const projectId = to.path.slice(idStart + 1, idStart + 22)
-
+    const idStart = projectsPath.length
+    const projectId = to.path.slice(idStart, idStart + 36)
     await projectStore.setSelectedProject(projectId)
   }
   next()
