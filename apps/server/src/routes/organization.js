9af953fcbf6c44370d14c3553320734d74f00c8a
Claire Nollet <clairenlet@gmail.com>
2023-01-20 17:29:02 +0100

    feat: :passport_control: organization controller

--- apps/server/src/routes/organization.js
+++ apps/server/src/routes/organization.js
@@ -1,9 +1,13 @@
 import {
   getOrganizationsController,
+  createOrganizationController,
 } from '../controllers/organization.js'
 
 const router = async (app, _opt) => {
+  // GET
   await app.get('/', getOrganizationsController)
+  // POST
+  await app.post('/', createOrganizationController)
 }
 
 export default router
--- apps/server/src/routes/project.spec.js
+++ apps/server/src/routes/project.spec.js
@@ -8,7 +8,7 @@ import { nanoid } from 'nanoid'
 import { sessionConf } from '../utils/keycloak.js'
 import { getConnection, closeConnections, sequelize } from '../connect.js'
 import { getProjectModel } from '../models/models.js'
-import projectRouter from './models.js'
+import projectRouter from './project.js'
 
 vi.mock('fastify-keycloak-adapter', () => ({ default: fp(async () => vi.fn()) }))
 vi.mock('../ansible.js')
