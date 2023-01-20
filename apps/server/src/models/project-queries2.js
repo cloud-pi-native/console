8ee273a76175f546fabbb5711b01ea358a7f464e
Claire Nollet <clairenlet@gmail.com>
2023-01-20 15:05:04 +0100

    feat: :card_file_box: add destroy functions for db

--- apps/server/src/models/project-queries2.js
+++ apps/server/src/models/project-queries2.js
@@ -17,6 +17,10 @@ export const getUserProjects = async (userId) => {
   return res
 }
 
+export const getProjectById = async (id) => {
+  return await getProjectModel().findByPk(id)
+}
+
 export const getProject = async ({ name, organization }) => {
   const res = await getProjectModel().findAll({
     raw: true,
@@ -33,12 +37,14 @@ export const projectInitializing = async ({ name, organization, ownerId }) => {
   const project = await getProject({ name, organization })
   if (project) throw Error('Un projet avec le nom et dans l\'organisation demandés existe déjà')
   const res = await getProjectModel().create({ name, organization, usersId: [ownerId], status: 'initializing', locked: true, ownerId })
-  return res?.dataValues
+  return res
 }
 
 // UPDATE
 
-// TODO : requête lockProject()
+export const projectLocked = async (id) => {
+  return await getProjectModel().update({ locked: true }, { where: { id } })
+}
 
 export const projectCreated = async ({ name, organization }) => {
   return await getProjectModel().update({ locked: false, status: 'created' }, { where: { name, organization } })
@@ -48,27 +54,38 @@ export const projectFailed = async ({ name, organization }) => {
   return await getProjectModel().update({ locked: false, status: 'failed' }, { where: { name, organization } })
 }
 
-export const projectAddUser = async ({ name, organization, userId }) => {
-  const project = await getProject({ name, organization })
-  const users = project.dataValues.usersId ?? [project.dataValues.ownerId]
+export const projectAddUser = async ({ projectId, userId }) => {
+  const project = await getProjectById(projectId)
+  const users = project.usersId ?? [project.ownerId]
   if (users.includes(userId)) throw Error('Cet utilisateur est déjà membre du projet')
   return await getProjectModel().update({
     usersId: sequelize.fn('array_append', sequelize.col('usersId'), userId),
   }, {
-    where: { name, organization },
+    where: { id: projectId },
   })
 }
 
-// TODO : requête removeUser
+// TODO : controller : suppression permissions associées
+export const projectRemoveUser = async ({ projectId, userId }) => {
+  const project = await getProjectById(projectId)
+  const users = project.usersId ?? [project.ownerId]
+  if (!users.includes(userId)) throw Error('Cet utilisateur n\'est pas membre du projet')
+  return await getProjectModel().update({
+    // usersId: sequelize.fn('array_append', sequelize.col('usersId'), userId),
+    // TODO : retirer le userId de l'array
+  }, {
+    where: { id: projectId },
+  })
+}
 
-export const projectArchiving = async ({ name, organization }) => {
-  const project = await getProject({ name, organization })
+export const projectArchiving = async (id) => {
+  const project = await getProjectById(id)
   if (!project) throw Error('Projet non trouvé')
   return await getProjectModel().update({
     status: 'archived',
     locked: true,
   }, {
-    where: { name, organization },
+    where: { id },
   })
 }
 
