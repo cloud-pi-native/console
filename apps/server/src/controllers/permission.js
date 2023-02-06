38e74d5adb84f285a9e72814156de5f69c60e4ce
Arnaud TARDIF <atardif@adlere.fr>
2023-02-06 12:04:20 +0100

    feat: :sparkles: keycloak manager

--- apps/server/src/controllers/permission.js
+++ apps/server/src/controllers/permission.js
@@ -53,6 +53,10 @@ export const setPermissionController = async (req, res) => {
     if (!role) throw new Error('Vous n\'êtes pas membre du projet')
 
     const permission = await setPermission({ userId: data.userId, environmentId, level: data.level })
+    // TODO chercher le noms de l'environnement associé et dériver les noms keycloak
+    // if (data.level === 0) await removeMembers([data.userId], [permission.Environment.name])
+    // if (data.level === 10) await removeMembers([data.userId], [permission.Environment.name]) && await addMembers([data.userId], [permission.Environment.name])
+    // if (data.level === 20) await addMembers([data.userId], [permission.Environment.name])
     req.log.info({
       ...getLogInfos(),
       description: 'Permission successfully created',
