642b245a4a0ffa8877c900fa786345fd484f60f8
Arnaud Tardif <atardif@adlere.fr>
2023-02-21 15:37:37 +0100

    feat: :sparkles: add log table in model

---
+++ apps/server/src/models/queries/log-queries.js
@@ -0,0 +1,31 @@
+import { sequelize } from '../../connect.js'
+import { getLogModel } from '../log.js'
+
+// SELECT
+export const getAllLogsForUser = async (offset = 0, user) => {
+  const res = await getLogModel().findAll({
+    where: {
+      user,
+    },
+    limit: 100,
+    offset,
+  })
+  return res
+}
+
+export const getAllLogs = async (offset = 0) => {
+  const res = await getLogModel().findAll({
+    limit: 100,
+    offset,
+  })
+  return res
+}
+
+// TECH
+export const _dropLogsTable = async () => {
+  await sequelize.drop({
+    tableName: getLogModel().tableName,
+    force: true,
+    cascade: true,
+  })
+}
