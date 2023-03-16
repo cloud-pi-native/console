b2bfd26dcb733acbbac9f94c11173609d88cddad
Claire Nollet <clairenlet@gmail.com>
2023-03-16 15:34:20 +0100

    test: :white_check_mark: fix services test

--- apps/server/src/utils/services.spec.js
+++ apps/server/src/utils/services.spec.js
@@ -6,8 +6,11 @@ describe('services', () => {
     const project = {
       id: 'thisIsAnId',
       name: 'myProjectName',
-      organization: {
-        name: 'myOrganization',
+      organization: 'myOrganization',
+      Organization: {
+        dataValues: {
+          name: 'myOrganization',
+        },
       },
     }
 
@@ -16,7 +19,7 @@ describe('services', () => {
     expect(populatedProject.services.length).toEqual(6)
     expect(populatedProject).toHaveProperty('id', project.id)
     expect(populatedProject).toHaveProperty('name', project.name)
-    expect(populatedProject).toHaveProperty('organization', project.organization)
+    expect(populatedProject).toHaveProperty('organization', project.Organization.dataValues.name)
     populatedProject.services.forEach(service => {
       expect(service).toHaveProperty('id')
       expect(service).toHaveProperty('title')
