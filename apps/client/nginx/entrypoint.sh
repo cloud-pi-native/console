8281ada269de23cc28d48c76b55bfaebcd50640a
Arnaud Tardif <atardif@adlere.fr>
2023-03-16 12:02:33 +0100

    fix: :fire: remove useless <SERVICE>_URL

--- apps/client/nginx/entrypoint.sh
+++ apps/client/nginx/entrypoint.sh
@@ -1,40 +1,34 @@
-#!/bin/sh
+    #!/bin/sh
 
-ROOT_DIR=/usr/share/nginx/html
-ENV_DIR=/env/
+    ROOT_DIR=/usr/share/nginx/html
+    ENV_DIR=/env/
 
-populate () {
-  if [ -z $(eval "echo \${$1}") ]; then
-    VAR="$(grep "^$1" $ENV_DIR/.env | xargs)"
-    export "$(grep "^$1" $ENV_DIR/.env | xargs)"
-  fi
+    populate () {
+      if [ -z $(eval "echo \${$1}") ]; then
+        VAR="$(grep "^$1" $ENV_DIR/.env | xargs)"
+        export "$(grep "^$1" $ENV_DIR/.env | xargs)"
+      fi
+      
+      KEY=$(echo "$1" | tr '[:upper:]' '[:lower:]' | sed 's/_/-/g')
+      VALUE=$(eval "echo \${$1}")
+      sed -i 's|'${KEY}'|'${VALUE}'|g' $2
+    }
 
-  KEY=$(echo "$1" | tr '[:upper:]' '[:lower:]' | sed 's/_/-/g')
-  VALUE=$(eval "echo \${$1}")
-  sed -i 's|'${KEY}'|'${VALUE}'|g' $2
-}
 
+    echo "Replacing env constants in JS"
+    for file in $ROOT_DIR/assets/index*.js; do
+      echo "Processing $file ...";
 
-echo "Replacing env constants in JS"
-for file in $ROOT_DIR/assets/index*.js; do
-  echo "Processing $file ...";
+      populate SERVER_HOST $file
+      populate SERVER_PORT $file
+      populate KEYCLOAK_PROTOCOL $file
+      populate KEYCLOAK_DOMAIN $file
+      populate KEYCLOAK_REALM $file
+      populate KEYCLOAK_CLIENT_ID $file
+      populate KEYCLOAK_REDIRECT_URI $file
+    done
 
-  populate SERVER_HOST $file
-  populate SERVER_PORT $file
-  populate KEYCLOAK_PROTOCOL $file
-  populate KEYCLOAK_DOMAIN $file
-  populate KEYCLOAK_REALM $file
-  populate KEYCLOAK_CLIENT_ID $file
-  populate KEYCLOAK_REDIRECT_URI $file
-  populate ARGOCD_URL $file
-  populate GITLAB_URL $file
-  populate NEXUS_URL $file
-  populate QUAY_URL $file
-  populate SONARQUBE_URL $file
-  populate VAULT_URL $file
-done
+    export SERVER="$SERVER_HOST:$SERVER_PORT"
+    cat /default.conf.tpl | envsubst '$SERVER' > /etc/nginx/conf.d/default.conf
 
-export SERVER="$SERVER_HOST:$SERVER_PORT"
-cat /default.conf.tpl | envsubst '$SERVER' > /etc/nginx/conf.d/default.conf
-
-nginx -g 'daemon off;'
+    nginx -g 'daemon off;'
