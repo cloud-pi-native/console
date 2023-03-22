85eef144f8c87760c61203de7944229af2828047
this-is-tobi <thibault_colin@hotmail.fr>
2023-03-22 15:36:51 +0100

    fix: :bug: use proxy on services healthcheck

--- apps/server/src/controllers/service.js
+++ apps/server/src/controllers/service.js
@@ -2,8 +2,14 @@ import { getLogInfos } from '../utils/logger.js'
 import { send200, send400 } from '../utils/response.js'
 import { allServices } from '../utils/services.js'
 import { getUserById } from '../models/queries/user-queries.js'
+import HttpsProxyAgent from 'https-proxy-agent'
+
+import https from 'node:https'
+import url from 'node:url'
 
 // GET
+const getProxyAgent = () => process.env.HTTPS_PROXY ? new HttpsProxyAgent(process.env.HTTPS_PROXY) : undefined
+
 export const checkServicesHealthController = async (req, res) => {
   const requestorId = req.session?.user?.id
 
@@ -11,17 +17,23 @@ export const checkServicesHealthController = async (req, res) => {
     const user = getUserById(requestorId)
     if (!user) throw new Error('Vous n\'avez pas accès à cette information')
 
-    const serviceData = await Promise
-      .all(Object.values(allServices)
-        .map(async service => {
-          const serviceRes = await fetch(service.url)
-          return ({
-            id: service.id,
-            status: serviceRes.status < 400 ? 'success' : 'error',
-            message: serviceRes?.statusText,
-            code: serviceRes.status,
+    const serviceData = await Promise.all(Object.values(allServices)
+      .map(service => {
+        return new Promise((resolve, _reject) => {
+          const urlParsed = url.parse(service.url)
+          https.get({
+            ...urlParsed,
+            agent: getProxyAgent(),
+          }, (res) => {
+            resolve({
+              id: service.id,
+              status: res.statusCode < 400 ? 'success' : 'error',
+              message: res?.statusMessage,
+              code: res?.statusCode,
+            })
           })
-        }))
+        })
+      }))
     return send200(res, serviceData)
   } catch (error) {
     let message
