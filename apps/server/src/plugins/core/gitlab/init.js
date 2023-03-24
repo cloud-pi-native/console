7bf550b66136df0f5977b2fa336621b3e06f38b0
ArnaudTA <atardif@adlere.fr>
2023-03-24 17:37:40 +0100

    feat: :sparkles: gitlab -> group

--- apps/server/src/plugins/core/gitlab/init.js
+++ apps/server/src/plugins/core/gitlab/init.js
@@ -1,23 +1,8 @@
-// import { Gitlab } from '@gitbeaker/node'
-// import { gitlabToken, gitlabUrl } from '../../../utils/env.js'
-import app from '../../../app.js'
+import { getGroupRootId } from './index.js'
+import { createGroup, deleteGroup } from './group.js'
 
-// export const api = new Gitlab({ token: gitlabToken, host: gitlabUrl })
-
-let groupRootId
-export const getGroupRootId = async () => {
-  // groupRootId = groupRootId || (await api.Groups.search('forge-mi')).find(org => org.full_path === 'forge-mi/projects').id
-  return groupRootId
-}
-
-const createGroup = async (payload) => {
-  app.log.info({ message: 'création du groupe', payload })
-  // for (let i = 0; i < 10000000000; i++) {
-  //   const element = i
-  // }
-  return { status: 'OK' }
-}
-
-export const init = (m) => {
-  m.register('gitlab', 'projectCreate', createGroup, 'main')
+export const init = (register) => {
+  getGroupRootId()
+  register('gitlab', 'createProject', createGroup, 'main')
+  register('gitlab', 'archiveProject', deleteGroup, 'main')
 }
