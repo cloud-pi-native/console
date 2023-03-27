c250b5d6f25fb69ad9b33a43e0b88eae1f907a8e
ArnaudTA <atardif@adlere.fr>
2023-03-27 05:08:55 +0200

    fix: :closed_lock_with_key: add env var for service administration

--- apps/server/src/utils/env.js
+++ apps/server/src/utils/env.js
@@ -38,10 +38,6 @@ export const keycloakClientId = process.env.KEYCLOAK_CLIENT_ID
 
 export const keycloakClientSecret = process.env.KEYCLOAK_CLIENT_SECRET
 
-export const ansibleHost = process.env.ANSIBLE_HOST
-
-export const ansiblePort = process.env.ANSIBLE_PORT
-
 export const adminsUserId = process.env.ADMIN_KC_USER_ID ? process.env.ADMIN_KC_USER_ID.split(',') : []
 
 export const encryptionKey = process.env.ENCRYPTION_KEY
@@ -49,11 +45,23 @@ export const encryptionKey = process.env.ENCRYPTION_KEY
 export const argocdUrl = process.env.ARGOCD_URL
 
 export const gitlabUrl = process.env.GITLAB_URL
+export const gitlabToken = process.env.GITLAB_TOKEN
 
 export const nexusUrl = process.env.NEXUS_URL
 
 export const quayUrl = process.env.QUAY_URL
 
 export const sonarqubeUrl = process.env.SONARQUBE_URL
+export const sonarqubeApiUrl = process.env.SONAR_API_URL
+export const sonarqubeApiToken = process.env.SONAR_API_TOKEN
 
 export const vaultUrl = process.env.VAULT_URL
+export const vaultToken = process.env.VAULT_TOKEN
+
+export const harborUrl = process.env.HARBOR_API_URL
+export const harborUser = process.env.HARBOR_ADMIN
+export const harborPassword = process.env.HARBOR_PASSWORD
+
+export const projectPath = process.env.PROJECT_PATH?.split('/') || ['forge-mi', 'projects']
+
+export const mockPlugins = process.env.MOCK_PLUGINS === 'true'
