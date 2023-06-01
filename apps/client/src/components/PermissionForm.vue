fb72076f16f79df456392fc7f9a7129ab1655832
Claire Nollet <clairenlet@gmail.com>
2023-06-01 12:22:57 +0200

    feat: :children_crossing: improve UX on permission form

--- apps/client/src/components/PermissionForm.vue
+++ apps/client/src/components/PermissionForm.vue
@@ -21,6 +21,7 @@ const userStore = useUserStore()
 const snackbarStore = useSnackbarStore()
 const environment = ref(props.environment)
 const permissions = ref([])
+const permissionToUpdate = ref({})
 
 const project = computed(() => projectStore.selectedProject)
 const owner = computed(() => projectStore.selectedProjectOwner)
@@ -35,7 +36,7 @@ const usersToLicence = computed(() =>
 const suggestions = computed(() => usersToLicence.value.map(user => user.email))
 
 const setPermissions = () => {
-  permissions.value = environment.value.permissions.sort((a, b) => a.level <= b.level)
+  permissions.value = environment.value.permissions.sort((a, b) => a.user?.email >= b.user?.email)
 }
 
 const addPermission = async (userEmail) => {
@@ -47,12 +48,13 @@ const addPermission = async (userEmail) => {
   }
 }
 
-const updatePermission = async (userId, level) => {
+const updatePermission = async () => {
   try {
-    await projectPermissionStore.updatePermission(environment.value.id, { userId, level })
+    await projectPermissionStore.updatePermission(environment.value.id, permissionToUpdate.value)
   } catch (error) {
     snackbarStore.setMessage(error?.message, 'error')
   }
+  permissionToUpdate.value = {}
 }
 
 const deletePermission = async (userId) => {
@@ -79,53 +81,85 @@ onMounted(() => {
   <DsfrFieldset
     data-testid="permissionsFieldset"
     :legend="`Droits des utilisateurs sur l'environnement de ${environment?.name}`"
-    hint="Gérez les droits de lecture, écriture et suppression d'un membre du projet sur l'environnement sélectionné."
+    class="full-fieldset"
+    hint="Gérez les droits de lecture (r), écriture (w) et suppression (d) d'un membre du projet sur l'environnement sélectionné."
   >
-    <ul class="flex flex-col gap-2 items-center">
+    <DsfrAlert
+      v-if="!isPermitted"
+      data-testid="notPermittedAlert"
+      :description="`Vous n'avez aucun droit sur l'environnement de ${environment?.name}. Un membre possédant des droits sur cet environnement peut vous accréditer.`"
+      small
+      type="info"
+    />
+    <ul class="flex flex-col gap-2 items-center w-full">
       <li
         v-for="permission in permissions"
         :key="permission.id"
         :data-testid="`userPermissionLi-${permission.user?.email}`"
-        class="flex justify-between md:flex-row flex-col w-full"
+        class="p-4 flex justify-between content-center items-end gap-2 md:flex-row flex-col w-full"
       >
         <div>
+          <div
+            class="flex gap-2"
+          >
+            <v-icon
+              :name="permission.userId === owner.id ? 'ri-user-star-fill' : 'ri-user-fill'"
+              fill="var(--text-title-blue-france)"
+            />
+            <p
+              class="fr-text-title--blue-france"
+              data-testid="userEmail"
+            >
+              {{ permission.user.email }}
+            </p>
+          </div>
           <DsfrButton
-            class="ml-8"
-            secondary
             data-testid="deletePermissionBtn"
             :disabled="permission.userId === owner.id || !isPermitted"
             :title="permission.userId === owner.id ? 'Les droits du owner ne peuvent être retirés' : `Retirer les droits de ${permission.user.email}`"
-            :icon-only="true"
+            label="Supprimer la permission"
+            class="my-4"
+            secondary
             icon="ri-close-line"
             @click="deletePermission(permission.userId)"
           />
-          <span
-            class="p-4 mr-4"
-            data-testid="userEmail"
-          >
-            {{ permission.user.email }}
-          </span>
         </div>
-        <RangeInput
-          data-testid="permissionLevelRange"
-          label="Niveau de droits"
-          :level="permission.level"
-          :levels="levels"
-          required="required"
-          :disabled="permission.userId === owner.id || !isPermitted"
-          @update-level="updatePermission(permission.userId, $event)"
-        />
+        <div>
+          <RangeInput
+            data-testid="permissionLevelRange"
+            label="Niveau de droits"
+            :level="permission.level"
+            :levels="levels"
+            required="required"
+            :disabled="permission.userId === owner.id || !isPermitted"
+            @update-level="(event) => {
+              permissionToUpdate.userId = permission.userId
+              permissionToUpdate.level = event
+            }"
+          />
+          <DsfrButton
+            :data-testid="`${permission.userId}UpdatePermissionBtn`"
+            :disabled="permission.userId === owner.id || !isPermitted || permissionToUpdate.userId !== permission.userId"
+            :title="permission.userId === owner.id ? 'Les droits du owner ne peuvent être modifiés' : `Confirmer la modification des droits de ${permission.user.email}`"
+            label="Confirmer la modification"
+            class="my-4"
+            secondary
+            icon="ri-check-fill"
+            @click="updatePermission()"
+          />
+        </div>
       </li>
     </ul>
   </DsfrFieldset>
   <DsfrFieldset
-    v-if="usersToLicence.length"
+    class="full-fieldset"
     data-testid="newPermissionFieldset"
     legend="Accréditer un membre du projet"
+    :hint="usersToLicence.length ? `Entrez l'e-mail d'un membre du projet ${project.name}. Ex : ${usersToLicence[0].email}` : `Tous les membres du projet ${project.name} sont déjà accrédités.`"
   >
     <SuggestionInput
       data-testid="permissionSuggestionInput"
-      :disabled="!isPermitted"
+      :disabled="!isPermitted || !usersToLicence.length"
       :label="`E-mail de l'utilisateur à accréditer sur l'environnement de ${environment?.name}`"
       placeholder="prenom.nom@interieur.gouv.fr"
       :suggestions="suggestions"
@@ -133,3 +167,9 @@ onMounted(() => {
     />
   </DsfrFieldset>
 </template>
+
+<style>
+.full-fieldset div.fr-fieldset__element {
+  width: 70%;
+}
+</style>
