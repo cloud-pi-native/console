a409e287142aca6c0dbd75e9264cebe6f10daaa0
LouisGongora <l.gongora@actongroup.com>
2023-06-09 18:04:52 +0200

    feat: :construction: prisma migrate & install client

---
+++ apps/server/src/prisma/migrations/20230608113127_test_toto/migration.sql
@@ -0,0 +1,129 @@
+-- CreateTable
+CREATE TABLE "Environment" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "projectId" INTEGER NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'initializing',
+
+    CONSTRAINT "Environment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Log" (
+    "id" SERIAL NOT NULL,
+    "data" JSONB NOT NULL,
+    "action" TEXT NOT NULL DEFAULT '',
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Log_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Organization" (
+    "id" SERIAL NOT NULL,
+    "source" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "label" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Permission" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "environmentId" INTEGER NOT NULL,
+    "level" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Permission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "organizationId" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "locked" BOOLEAN NOT NULL DEFAULT false,
+    "services" JSONB NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Repository" (
+    "id" SERIAL NOT NULL,
+    "projectId" INTEGER NOT NULL,
+    "internalRepoName" TEXT NOT NULL,
+    "externalRepoUrl" TEXT NOT NULL,
+    "externalUserName" TEXT NOT NULL,
+    "externalToken" TEXT NOT NULL,
+    "isInfra" BOOLEAN NOT NULL DEFAULT false,
+    "isPrivate" BOOLEAN NOT NULL DEFAULT false,
+    "status" TEXT NOT NULL DEFAULT 'initializing',
+
+    CONSTRAINT "Repository_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UsersProjects" (
+    "UserId" INTEGER NOT NULL,
+    "ProjectId" INTEGER NOT NULL,
+    "role" TEXT NOT NULL,
+
+    CONSTRAINT "UsersProjects_pkey" PRIMARY KEY ("UserId","ProjectId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_id_key" ON "Organization"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_name_key" ON "Organization"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_label_key" ON "Organization"("label");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Permission_id_key" ON "Permission"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_id_key" ON "Project"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Environment" ADD CONSTRAINT "Environment_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Log" ADD CONSTRAINT "Log_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Environment"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Repository" ADD CONSTRAINT "Repository_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UsersProjects" ADD CONSTRAINT "UsersProjects_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UsersProjects" ADD CONSTRAINT "UsersProjects_ProjectId_fkey" FOREIGN KEY ("ProjectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ apps/server/src/prisma/migrations/20230608113854_add_table_test/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Test" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "Test_pkey" PRIMARY KEY ("id")
+);
