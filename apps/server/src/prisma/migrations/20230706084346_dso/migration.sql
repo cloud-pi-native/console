5ad09f880d5a1166eb9500b4b916e688c1709128
Claire Nollet <clairenlet@gmail.com>
2023-07-06 10:54:54 +0200

    refactor: :card_file_box: migration db

--- apps/server/src/prisma/migrations/20230706084346_dso/migration.sql
+++ apps/server/src/prisma/migrations/20230706084346_dso/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "Environments" (
+CREATE TABLE "Environment" (
     "id" UUID NOT NULL,
     "name" TEXT NOT NULL,
     "projectId" UUID NOT NULL,
@@ -7,11 +7,11 @@ CREATE TABLE "Environments" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Environments_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Environment_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Logs" (
+CREATE TABLE "Log" (
     "id" UUID NOT NULL,
     "data" JSONB NOT NULL,
     "action" TEXT NOT NULL DEFAULT '',
@@ -19,11 +19,11 @@ CREATE TABLE "Logs" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Logs_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Log_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Organizations" (
+CREATE TABLE "Organization" (
     "id" UUID NOT NULL,
     "source" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -32,11 +32,11 @@ CREATE TABLE "Organizations" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Organizations_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Permissions" (
+CREATE TABLE "Permission" (
     "id" UUID NOT NULL,
     "userId" UUID NOT NULL,
     "environmentId" UUID NOT NULL,
@@ -44,11 +44,11 @@ CREATE TABLE "Permissions" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Permissions_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Permission_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Projects" (
+CREATE TABLE "Project" (
     "id" UUID NOT NULL,
     "name" TEXT NOT NULL,
     "organizationId" UUID NOT NULL,
@@ -59,11 +59,11 @@ CREATE TABLE "Projects" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Projects_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Repositories" (
+CREATE TABLE "Repository" (
     "id" UUID NOT NULL,
     "projectId" UUID NOT NULL,
     "internalRepoName" TEXT NOT NULL,
@@ -76,11 +76,11 @@ CREATE TABLE "Repositories" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Repositories_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Repository_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Users" (
+CREATE TABLE "User" (
     "id" UUID NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
@@ -88,64 +88,64 @@ CREATE TABLE "Users" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Roles" (
+CREATE TABLE "Role" (
     "userId" UUID NOT NULL,
     "projectId" UUID NOT NULL,
     "role" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Roles_pkey" PRIMARY KEY ("userId","projectId")
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("userId","projectId")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Organizations_id_key" ON "Organizations"("id");
+CREATE UNIQUE INDEX "Organization_id_key" ON "Organization"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Organizations_name_key" ON "Organizations"("name");
+CREATE UNIQUE INDEX "Organization_name_key" ON "Organization"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Organizations_label_key" ON "Organizations"("label");
+CREATE UNIQUE INDEX "Organization_label_key" ON "Organization"("label");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_id_key" ON "Permissions"("id");
+CREATE UNIQUE INDEX "Permission_id_key" ON "Permission"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_userId_environmentId_key" ON "Permissions"("userId", "environmentId");
+CREATE UNIQUE INDEX "Permission_userId_environmentId_key" ON "Permission"("userId", "environmentId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Projects_id_key" ON "Projects"("id");
+CREATE UNIQUE INDEX "Project_id_key" ON "Project"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Roles_userId_projectId_key" ON "Roles"("userId", "projectId");
+CREATE UNIQUE INDEX "Role_userId_projectId_key" ON "Role"("userId", "projectId");
 
 -- AddForeignKey
-ALTER TABLE "Environments" ADD CONSTRAINT "Environments_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Projects"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Environment" ADD CONSTRAINT "Environment_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Logs" ADD CONSTRAINT "Logs_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Log" ADD CONSTRAINT "Log_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Environments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "Environment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Projects" ADD CONSTRAINT "Projects_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Project" ADD CONSTRAINT "Project_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Repositories" ADD CONSTRAINT "Repositories_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Projects"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Repository" ADD CONSTRAINT "Repository_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Roles" ADD CONSTRAINT "Roles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Role" ADD CONSTRAINT "Role_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Roles" ADD CONSTRAINT "Roles_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Projects"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Role" ADD CONSTRAINT "Role_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
