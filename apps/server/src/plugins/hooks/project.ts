7e97505b5a75c3f6c680d5b1bb6f18a9e45b87b3
Claire Nollet <clairenlet@gmail.com>
2023-07-18 14:14:37 +0200

    feat: :alien: add updateProject hook for description update for example

--- apps/server/src/plugins/hooks/project.ts
+++ apps/server/src/plugins/hooks/project.ts
@@ -4,11 +4,13 @@ import { type Hook, createHook } from './hook.js'
 
 export type Project = string
 export type ProjectBase = { organization: Organization, project: Project }
-export type ProjectInfos = { description: string, status: ProjectModel['status'] }
 export type CreateProjectValidateArgs = { owner: UserModel }
-export type CreateProjectExecArgs = CreateProjectValidateArgs & ProjectBase & ProjectInfos
+export type CreateProjectExecArgs = CreateProjectValidateArgs & ProjectBase & { description: string }
+export type UpdateProjectValidateArgs = ProjectBase & { description: string, status: ProjectModel['status'] }
+export type UpdateProjectExecArgs = ProjectBase & { description: string, status: ProjectModel['status'] }
 export type ArchiveProjectValidateArgs = ProjectBase
-export type ArchiveProjectExecArgs = ProjectBase
+export type ArchiveProjectExecArgs = ProjectBase & { status: ProjectModel['status'] }
 
 export const createProject: Hook<CreateProjectExecArgs, CreateProjectValidateArgs> = createHook()
+export const updateProject: Hook<UpdateProjectExecArgs, UpdateProjectValidateArgs> = createHook()
 export const archiveProject: Hook<ArchiveProjectExecArgs, ArchiveProjectValidateArgs> = createHook()
