eb0518462635460c4cbd5d40d137164298e62fad
Claire Nollet <clairenlet@gmail.com>
2023-08-18 17:02:42 +0200

    refactor: :recycle: business logic for log controller

--- apps/server/src/resources/log/admin/business.ts
+++ apps/server/src/resources/log/admin/business.ts
@@ -1,1 +1,6 @@
-// TODO 539
+import { getAllLogs } from '@/resources/queries-index.js'
+import { AdminLogsGet } from 'shared'
+
+export const getAllLogsBusiness = async (offset: AdminLogsGet['query']['offset'], limit: AdminLogsGet['query']['limit']) => {
+  return await getAllLogs({ offset, limit })
+}
