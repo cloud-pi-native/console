75e97d4002c0f285fcb3b11d10dd383cce7611c9
this-is-tobi <thibault_colin@hotmail.fr>
2023-08-29 20:07:04 +0200

    ci: :construction_worker: improve CI maintenance

--- ci/act/scripts/run-ci-locally.sh
+++ ci/act/scripts/run-ci-locally.sh
@@ -9,7 +9,7 @@ i=1
 # Get project directories
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
 ACT_DIR="$PROJECT_DIR/ci/act"
-ACT_ENV_FILE="$PROJECT_DIR/ci/act/env/.env"
+ACT_ENV_FILE="$ACT_DIR/env/.env"
 REGISTRY_DIR="$ACT_DIR/docker/registry"
 
 # Get versions
@@ -18,10 +18,11 @@ DOCKER_VERSION="$(docker --version)"
 DOCKER_COMPOSE_VERSION="$(docker compose version)"
 
 # Get Date
-NOW=$(date +'%Y-%m-%dT%H-%M-%SZ')
+NOW=$(date +'%Y-%m-%dT%H-%M-%S')
 
 # Default
 EVENT_FILE="$ACT_DIR/events/pr_base_main.json"
+START_REGISTRY="false"
 WORKFLOW_DIR="$ACT_DIR/workflows/"
 
 # Env & secrets
@@ -34,6 +35,9 @@ Following flags are available:
   -e    (Optional) Event file in './ci/act/events/' that will trigger workflows. e.g: './ci/act/events/push_base_main.json'.
         Default is '$EVENT_FILE'.
 
+  -r    (Optional) Start a local registry running as a docker container.
+        Default is '$START_REGISTRY'.
+
   -w    (Optional) Workflow directory that will be triggered. e.g: './github/worlflows' or './ci/act/workflows/test'.
         Default is '$WORKFLOW_DIR'.
 
@@ -44,11 +48,12 @@ print_help() {
 }
 
 # Parse options
-while getopts he:w: flag
-do
+while getopts he:rw: flag; do
   case "${flag}" in
     e)
       EVENT_FILE="$(readlink -f ${OPTARG})";;
+    r)
+      START_REGISTRY="true";;
     w)
       WORKFLOW_DIR="$(readlink -f ${OPTARG})";;
     h | *)
@@ -59,9 +64,9 @@ done
 
 # utils
 install_act() {
-  printf "\n${red}Optional.${no_color} Installs act...\n"
+  printf "\n\n${red}Optional.${no_color} Installs act...\n\n"
   curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
-  printf "\nact version $(act --version) installed\n"
+  printf "\n\nact version $(act --version) installed\n\n"
 }
 
 if [ -z "$(act --version)" ]; then
@@ -81,61 +86,71 @@ fi
 
 # Script condition
 if [ ! -d "$WORKFLOW_DIR" ]; then
-  printf "\nWorkflow directory '$WORKFLOW_DIR' does not exist.\n"
+  printf "\n\nWorkflow directory '$WORKFLOW_DIR' does not exist.\n\n"
   print_help
   exit 1
 fi
 
 # Settings
 EVENT_NAME=$(cat "$EVENT_FILE" | jq -r 'keys_unsorted | first')
-printf "\nScript settings:
+printf "\n\nScript settings:
   -> act version: ${ACT_VERSION}
   -> docker version: ${DOCKER_VERSION}
   -> docker-compose version: ${DOCKER_COMPOSE_VERSION}
   -> workflows directory: ${WORKFLOW_DIR}
   -> event file: ${EVENT_FILE}
-  -> event name: ${EVENT_NAME}\n"
-
-
-
-# printf "\n${red}${i}.${no_color} Create credentials for local registry\n\n"
-# i=$(($i + 1))
-
-# docker run \
-#   --rm \
-#   --entrypoint htpasswd \
-#   httpd:2 -Bbn "$REGISTRY_USERNAME" "$REGISTRY_SECRET" \
-#   > "$REGISTRY_DIR/auth/htpasswd"
-
-
-printf "\n${red}${i}.${no_color} Start local registry\n\n"
-i=$(($i + 1))
-
-docker compose -f ./ci/act/docker/registry/docker-compose.registry.yml up -d
-
-
-printf "\n${red}${i}.${no_color} Builds docker image use by act as Github runner\n\n"
-i=$(($i + 1))
-
-cd $ACT_DIR/docker
-docker build \
-  --tag "$REGISTRY_HOST/act/ubuntu" \
-  .
-
-docker push "$REGISTRY_HOST/act/ubuntu"
-
-
-printf "\n${red}${i}.${no_color} Displays workflow list\n\n"
-i=$(($i + 1))
+  -> event name: ${EVENT_NAME}\n\n"
 
 cd $PROJECT_DIR
+
+if [ "$START_REGISTRY" = "true" ]; then
+  printf "\n\n${red}${i}.${no_color} Create certificate for local registry\n\n"
+  i=$(($i + 1))
+
+  openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+    -keyout $REGISTRY_DIR/certs/registry.local.key -out $REGISTRY_DIR/certs/registry.local.crt \
+    -subj "/C=FR/ST=France/L=Paris/O=IT/CN=registry.local"
+
+
+  printf "\n\n${red}${i}.${no_color} Add local certificate for the registry\n\n"
+  i=$(($i + 1))
+
+  touch /etc/ssl/certs/registry.local && cat $REGISTRY_DIR/certs/registry.local.crt > /etc/ssl/certs/registry.local
+
+
+  printf "\n\n${red}${i}.${no_color} Add the registry in docker daemon insecure registries\n\n"
+  i=$(($i + 1))
+
+  jq '."insecure-registries" += ["localhost:5555"]' ~/.docker/daemon.json > daemon.tmp \
+    && mv daemon.tmp ~/.docker/daemon.json
+
+
+  printf "\n\n${red}${i}.${no_color} Create credentials for local registry\n\n"
+  i=$(($i + 1))
+
+  docker run \
+    --rm \
+    --entrypoint htpasswd \
+    httpd:2 -Bbn "$REGISTRY_USERNAME" "$REGISTRY_SECRET" \
+    > "$REGISTRY_DIR/auth/htpasswd"
+
+  printf "\n\n${red}${i}.${no_color} Start local registry\n\n"
+  i=$(($i + 1))
+
+  docker compose -f $REGISTRY_DIR/docker-compose.registry.yml --env-file $ACT_ENV_FILE up -d
+fi
+
+
+printf "\n\n${red}${i}.${no_color} Displays workflow list\n\n"
+i=$(($i + 1))
+
 act \
   --workflows "$WORKFLOW_DIR" \
   --eventpath "$EVENT_FILE" \
   --list
 
 
-printf "\n${red}${i}.${no_color} Displays workflow graph\n\n"
+printf "\n\n${red}${i}.${no_color} Displays workflow graph\n\n"
 i=$(($i + 1))
 
 act \
@@ -144,25 +159,25 @@ act \
   --graph
 
 
-printf "\n${red}${i}.${no_color} Runs locally GitHub Actions workflow\n\n"
+printf "\n\n${red}${i}.${no_color} Runs locally GitHub Actions workflow\n\n"
 i=$(($i + 1))
 
-cd $PROJECT_DIR
 act "$EVENT_NAME" \
-  --platform "ubuntu-latest=localhost:6000/act/ubuntu" \
+  --platform "ubuntu-latest=ghcr.io/this-is-tobi/tools/act-runner:latest" \
   --workflows "$WORKFLOW_DIR" \
   --eventpath "$EVENT_FILE" \
   --use-gitignore \
   --artifact-server-path "$ACT_DIR/artifacts" \
   --env "GITHUB_RUN_ID=$NOW" \
   --env "REGISTRY_HOST=$REGISTRY_HOST" \
+  --env "REGISTRY_PORT=$REGISTRY_PORT" \
+  --secret REGISTRY_USERNAME=$REGISTRY_USERNAME \
+  --secret REGISTRY_SECRET=$REGISTRY_SECRET \
   --rm
   # --bind \
-  # --secret REGISTRY_USERNAME=$REGISTRY_USERNAME \
-  # --secret REGISTRY_SECRET=$REGISTRY_SECRET
 
 
-printf "\n${red}${i}.${no_color} Retrieves artifacts\n\n"
+printf "\n\n${red}${i}.${no_color} Retrieves artifacts\n\n"
 i=$(($i + 1))
 
 if [ -d "$ACT_DIR/artifacts/$NOW" ] && [ -n "$(find $ACT_DIR/artifacts/$NOW -type f -name '*.gz__')" ]; then
@@ -173,13 +188,15 @@ if [ -d "$ACT_DIR/artifacts/$NOW" ] && [ -n "$(find $ACT_DIR/artifacts/$NOW -typ
 fi
 
 
-printf "\n${red}${i}.${no_color} Stop local registry\n\n"
-i=$(($i + 1))
+if [ "$START_REGISTRY" = "true" ]; then
+  printf "\n\n${red}${i}.${no_color} Stop local registry\n\n"
+  i=$(($i + 1))
 
-docker compose -f ./ci/act/docker/registry/docker-compose.registry.yml down -v
+  docker compose -f $REGISTRY_DIR/docker-compose.registry.yml down -v
+fi
 
 
-printf "\n${red}${i}.${no_color} Clean up\n\n"
+printf "\n\n${red}${i}.${no_color} Clean up\n\n"
 i=$(($i + 1))
 
 cat "$ACT_ENV_FILE" | while read e; do
