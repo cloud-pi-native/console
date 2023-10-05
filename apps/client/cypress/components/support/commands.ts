970ab7da03f7672f3641e9fd2ad03593a2a24e8d
this-is-tobi <thibault_colin@hotmail.fr>
2023-10-05 18:16:42 +0200

    test: :label: add types for cypress

--- apps/client/cypress/components/support/commands.ts
+++ apps/client/cypress/components/support/commands.ts
@@ -1,6 +1,28 @@
 import { mount } from '@cypress/vue'
+import VueDsfr from '@gouvminint/vue-dsfr'
 
-Cypress.Commands.add('mount', mount)
+import * as icons from '@/icons'
+
+import 'virtual:uno.css'
+import 'uno.css'
+import 'virtual:unocss-devtools'
+import '@/main.css'
+// import { ComponentOptions, ComponentOptionsWithObjectProps } from 'vue'
+
+Cypress.Commands.add('mount', (component, options = {}) => {
+  // Setup options object
+  options.global = options.global || {}
+  options.global.components = options.global.components || {}
+  options.global.plugins = options.global.plugins || []
+
+  options.global.plugins.push({
+    install (app) {
+      app.use(VueDsfr, { icons: Object.values(icons) })
+    },
+  })
+
+  return mount(component, options)
+})
 
 Cypress.Commands.add('getByDataTestid', (dataTestid) => {
   cy.get(`[data-testid="${dataTestid}"]`)
--- apps/client/cypress/components/support/index.ts
+++ apps/client/cypress/components/support/index.ts
@@ -1,1 +1,23 @@
 import './commands.js'
+import { mount } from 'cypress/vue'
+
+type MountParams = Parameters<typeof mount>
+type OptionsParam = MountParams[1]
+
+declare global {
+
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  module Cypress {
+    interface Chainable {
+      /**
+       * Helper mount function for Vue Components
+       * @param component Vue Component or JSX Element to mount
+       * @param options Options passed to mount the component
+       */
+      mount(
+        component: any,
+        options?: OptionsParam
+      ): Chainable<any>
+    }
+  }
+}
