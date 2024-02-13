aca3a54a87c939236babf83051c64f7a992f0f02
this-is-tobi <thibault_colin@hotmail.fr>
2024-02-13 17:04:52 +0100

    refactor: :label: add types on shared package

--- packages/shared/src/utils/schemas.ts
+++ packages/shared/src/utils/schemas.ts
@@ -1,40 +1,43 @@
 import Joi from 'joi'
 
 type Context = {
+  key?: string,
+  projectNameMaxLength?: number
+}
+type FullContext = {
   keysToValidate?: string[],
-  context?: {
-    key?: string,
-    projectNameMaxLength?: number
-  }
+  context?: Context
 }
 
 /**
  * @param {object} schema Schema used for validation
  * @param {object} data Data to test
+ * @param {object} [ctx] Object with list of keys to validate and context
  * @returns {object} Validation error object
- */
-// @ts-ignore
-export const schemaValidator = (schema: Record<string, any>, data: Record<string, any>, { keysToValidate, context }: Context = {}): Joi.ValidationError => {
+*/
+export const schemaValidator = (schema: Joi.Schema, data: Record<string, unknown> | undefined, { keysToValidate, context }: FullContext = {}): Record<string, Joi.ValidationError> => {
   const validation = schema.validate(data, { abortEarly: false, context }).error?.details || []
   return validation
-    .filter((error) => keysToValidate ? keysToValidate.includes(error.context.key) : true)
-    .reduce((acc, cur) => ({ ...acc, [cur.context.key]: cur.message }), {})
+    .filter((error) => keysToValidate && error.context?.key ? keysToValidate.includes(error.context.key) : true)
+    .reduce((acc, cur) => ({ ...acc, [cur.context?.key || 'unknown']: cur.message }), {})
 }
 
 /**
  * @param {object} schema Schema used for validation
  * @param {object} data Data to test
  * @param {object} key Key to test
+ * @param {object} [ctx] Object with list of keys to validate and context
  * @returns {boolean} Is valid key
  */
-export const isValid = (schema, data, key, ctx?) => !schemaValidator(schema, data, { context: ctx })[key]
+export const isValid = (schema: Joi.Schema, data: Record<string, unknown>, key: string, ctx?: Context) => !schemaValidator(schema, data, { context: ctx })[key]
 
 /**
  * @param {object} model Schema that will be parse
  * @returns {object} Result parsed schema
  */
+// @ts-ignore Joi will be replace by Zod
 const parseJoi = (model, value) => Array.from(model.schema._ids._byKey)
-// @ts-ignore
+  // @ts-ignore
   .reduce((acc: Record<string, any>, [key, val]) => ({ ...acc, [key]: instanciateSchema(val, value) }), {})
 
 /**
@@ -42,6 +45,7 @@ const parseJoi = (model, value) => Array.from(model.schema._ids._byKey)
  * @param {any} value Value passed to each key
  * @returns {object} Result schema with all values set to true
  */
+// @ts-ignore Joi will be replace by Zod
 export const instanciateSchema = (model, value) => {
   if (model.schema?.type === 'object') {
     return parseJoi(model, value)
