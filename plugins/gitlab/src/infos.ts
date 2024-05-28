28d3ebce4d6912904e1f21cd3c126ca342fc3cfb
ArnaudTa <33383276+ArnaudTA@users.noreply.github.com>
2024-05-28 10:59:51 +0200

    feat: :sparkles: option for gitlab display secret

--- plugins/gitlab/src/infos.ts
+++ plugins/gitlab/src/infos.ts
@@ -1,12 +1,28 @@
 import type { ServiceInfos } from '@cpn-console/hooks'
 import { getConfig } from './utils.js'
+import { ENABLED } from '@cpn-console/shared'
 
-const infos: ServiceInfos = {
+const infos = {
   name: 'gitlab',
   to: ({ project, organization }) => `${getConfig().url}/${getConfig().projectsRootDir}/${organization}/${project}`,
   title: 'Gitlab',
   imgSrc: '/img/gitlab.svg',
   description: 'GitLab est un service d\'hébergement de code source et de pipeline CI/CD',
-}
+  config: {
+    global: [{
+      kind: 'switch',
+      key: 'displayTriggerHint',
+      initialValue: ENABLED,
+      permissions: {
+        admin: { read: true, write: true },
+        user: { read: false, write: false },
+      },
+      title: 'Afficher l\'aide de trigger de pipeline',
+      value: ENABLED,
+      description: 'Afficher l\'aide de trigger de pipeline aux utilisateurs lorsqu\'ils souhaitent afficher les secrets du projet',
+    }],
+    project: [],
+  },
+} as const satisfies ServiceInfos
 
 export default infos
