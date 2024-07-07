7d8f89710251f708829af4fdf4c67a7058da9811
ArnaudTa <33383276+ArnaudTA@users.noreply.github.com>
2024-07-08 00:35:29 +0200

    test: :white_check_mark: change path for e2e tests

--- apps/client/cypress/e2e/specs/admin/quotas.e2e.ts
+++ apps/client/cypress/e2e/specs/admin/quotas.e2e.ts
@@ -47,7 +47,7 @@ describe('Administration quotas', () => {
     cy.intercept('GET', 'api/v1/clusters').as('getClusters')
     cy.intercept('GET', 'api/v1/stages').as('getStages')
     cy.intercept('GET', 'api/v1/quotas').as('getQuotas')
-    cy.intercept('GET', '/api/v1/projects').as('getProjects')
+    cy.intercept('GET', '/api/v1/projects/mines').as('getProjects')
     cy.intercept('GET', '/api/v1/environments?*').as('getEnvironments')
 
     // Create quota
@@ -221,7 +221,7 @@ describe('Administration quotas', () => {
     cy.intercept('GET', 'api/v1/clusters').as('getClusters')
     cy.intercept('GET', 'api/v1/stages').as('getStages')
     cy.intercept('GET', 'api/v1/quotas').as('getQuotas')
-    cy.intercept('GET', '/api/v1/projects').as('getProjects')
+    cy.intercept('GET', '/api/v1/projects/mines').as('getProjects')
     cy.intercept('GET', '/api/v1/environments?*').as('getEnvironments')
 
     // Create quota
@@ -346,7 +346,7 @@ describe('Administration quotas', () => {
     cy.intercept('GET', 'api/v1/clusters').as('getClusters')
     cy.intercept('GET', 'api/v1/stages').as('getStages')
     cy.intercept('GET', 'api/v1/quotas').as('getQuotas')
-    cy.intercept('GET', '/api/v1/projects').as('getProjects')
+    cy.intercept('GET', '/api/v1/projects/mines').as('getProjects')
 
     cy.getByDataTestid(`quotaTile-${publicQuota.name}`)
       .should('be.visible')
