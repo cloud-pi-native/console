3372a7c38070188053ba444e0c144034377464dd
this-is-tobi <this-is-tobi@proton.me>
2024-08-14 23:38:10 +0200

    build: :arrow_up: upgrade nodejs to v20.16.0

--- apps/client/cypress/components/specs/repo-form.ct.ts
+++ apps/client/cypress/components/specs/repo-form.ct.ts
@@ -71,7 +71,7 @@ describe('RepoForm.vue', () => {
     cy.getByDataTestid('gitlabCIAccordion').click()
     cy.get('select#type-language-select').should('be.visible')
       .select('node')
-    cy.getByDataTestid('nodeVersionInput').type('20.15.1')
+    cy.getByDataTestid('nodeVersionInput').type('20.16.0')
     cy.getByDataTestid('nodeInstallInput').type('npm install')
     cy.getByDataTestid('nodeBuildInput').type('npm build')
     cy.getByDataTestid('workingDirInput').type('./')
