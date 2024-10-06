8c6cd318979c967dc1ba1a2fd19b2149417e74ca
ArnaudTa <33383276+ArnaudTA@users.noreply.github.com>
2024-10-06 20:32:00 +0200

    build: :construction_worker: create a tag for each commit on pr

--- ci/scripts/delete-image.sh
+++ ci/scripts/delete-image.sh
@@ -20,7 +20,9 @@ Following flags are available:
 
   -o    Github organization used for api calls.
 
-  -t    Image tage to delete.
+  -s    Image tag to search for delete.
+
+  -t    Image tag to delete.
 
   -h    Print script help.\n\n"
 
@@ -29,7 +31,7 @@ print_help() {
 }
 
 # Parse options
-while getopts hg:i:o:t: flag; do
+while getopts hg:i:o:s:t: flag; do
   case "${flag}" in
     g)
       GITHUB_TOKEN=${OPTARG};;
@@ -37,6 +39,8 @@ while getopts hg:i:o:t: flag; do
       IMAGE_NAME=${OPTARG};;
     o)
       ORG=${OPTARG};;
+    s)
+      SEARCH_TAG=${OPTARG};;
     t)
       TAG=${OPTARG};;
     h | *)
@@ -60,13 +64,35 @@ checkBuildxPlugin () {
   fi
 }
 
+deleteTag () {
+  MAIN_IMAGE_ID=$(echo "$IMAGES" | jq -r --arg t "$1" '.[] | select(.metadata.container.tags[] | contains($t)) | .id')
+  # Delete subsequent images
+  while read -r SHA; do
+    IMAGE_ID=$(echo "$IMAGES" | jq -r --arg s "$SHA" '.[] | select(.name==$s) | .id')
+
+    printf "\n${red}[Delete ghcr image].${no_color} Deleting subsequent image '$ORG/$IMAGE_NAME@$SHA'\n"
+    # curl -s \
+    #   -X DELETE \
+    #   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+    #   "https://api.github.com/orgs/${ORG}/packages/container/${IMAGE_NAME_URL_ENCODED}/versions/${IMAGE_ID}"
+  done <<< "$(docker buildx imagetools inspect ghcr.io/${ORG}/${IMAGE_NAME}:$1 --raw | jq -r '.manifests[] | .digest')"
+
+  # Delete main image
+  printf "\n${red}[Delete ghcr image].${no_color} Deleting image '$ORG/$IMAGE_NAME:$1'\n"
+
+  curl -s \
+    -X DELETE \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+    "https://api.github.com/orgs/${ORG}/packages/container/${IMAGE_NAME_URL_ENCODED}/versions/${MAIN_IMAGE_ID}"
+}
+
 # Settings
 printf "\nScript settings:
   -> docker version: ${DOCKER_VERSION}
   -> docker buildx version: ${DOCKER_BUILDX_VERSION}\n"
 
 
-if [ -z "$GITHUB_TOKEN" ] || [ -z "$ORG" ] || [ -z "$IMAGE_NAME" ] || [ -z "$TAG" ]; then
+if [ -z "$GITHUB_TOKEN" ] || [ -z "$ORG" ] || [ -z "$IMAGE_NAME" ]; then
   echo "\nYMissing arguments ...\n"
   print_help
   exit 1
@@ -76,24 +102,22 @@ IMAGE_NAME_URL_ENCODED="$(jq -rn --arg x ${IMAGE_NAME} '$x | @uri')"
 IMAGES=$(curl -s \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   "https://api.github.com/orgs/${ORG}/packages/container/${IMAGE_NAME_URL_ENCODED}/versions?per_page=100")
-MAIN_IMAGE_ID=$(echo "$IMAGES" | jq -r --arg t "$TAG" '.[] | select(.metadata.container.tags[] | contains($t)) | .id')
 
-# Delete subsequent images
-while read -r SHA; do
-  IMAGE_ID=$(echo "$IMAGES" | jq -r --arg s "$SHA" '.[] | select(.name==$s) | .id')
+set +e
 
-  printf "\n${red}[Delete ghcr image].${no_color} Deleting subsequent image '$ORG/$IMAGE_NAME@$SHA'\n"
+if [ ! -z "$TAG" ]; then
+  deleteTag $1
+fi
+if [ ! -z "$SEARCH_TAG" ]; then
+  echo Searching: $SEARCH_TAG
+  TAGS=$(echo "$IMAGES" | jq -r --arg t "$SEARCH_TAG" '.[] | select(.metadata.container.tags[] | select(contains($t))) | .metadata.container.tags | join(" ")')
+  echo Found tags: $TAGS
 
-  curl -s \
-    -X DELETE \
-    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-    "https://api.github.com/orgs/${ORG}/packages/container/${IMAGE_NAME_URL_ENCODED}/versions/${IMAGE_ID}"
-done <<< "$(docker buildx imagetools inspect ghcr.io/${ORG}/${IMAGE_NAME}:${TAG} --raw | jq -r '.manifests[] | .digest')"
-
-# Delete main image
-printf "\n${red}[Delete ghcr image].${no_color} Deleting image '$ORG/$IMAGE_NAME:$TAG'\n"
-
-curl -s \
-  -X DELETE \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "https://api.github.com/orgs/${ORG}/packages/container/${IMAGE_NAME_URL_ENCODED}/versions/${MAIN_IMAGE_ID}"
+  if [ ! -z "$TAGS" ]; then
+    for TAG in "$TAGS"; do
+      deleteTag $1
+    done
+  else
+    printf "\n${red}[Delete ghcr image].${no_color} No tags found\n"
+  fi
+fi
