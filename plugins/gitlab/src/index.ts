cea79efc6ecb4c33d868d7f5044ddc7825c69ed5
Mathieu LAUDE <mathieu.laude@wescale.fr>
2024-12-10 15:25:49 +0100

    feat: :sparkles: store cluster secret in Vault

--- plugins/gitlab/src/index.ts
+++ plugins/gitlab/src/index.ts
@@ -52,7 +52,7 @@ export const plugin: Plugin = {
     upsertZone: {
       api: () => new GitlabZoneApi(),
       steps: {
-        main: upsertZone,
+        pre: upsertZone,
         post: commitFiles,
       },
     },
