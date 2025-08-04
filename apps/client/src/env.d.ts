09045933dd64440878c19603070ade154f854311
Stéphane TRÉBEL (WeScale) <stephane.trebel@wescale.fr>
2025-08-04 15:32:38 +0200

    chore(client): add types for vue3-json-viewer

--- apps/client/src/env.d.ts
+++ apps/client/src/env.d.ts
@@ -13,3 +13,25 @@ declare module 'virtual:pwa-register/vue' {
     updateServiceWorker: (reloadPage?: boolean) => Promise<void>
   }
 }
+
+declare module 'vue3-json-viewer' {
+    import { AllowedComponentProps, App, Component, ComponentCustomProps, VNodeProps } from 'vue';
+
+    interface JsonViewerProps {
+        value: Record<string, unknown> | Array<any> | string | number | boolean;
+        expanded: boolean;
+        expandDepth: number;
+        copyable: boolean | object;
+        sort: boolean;
+        boxed: boolean;
+        theme: string; //"dark" | "light"
+        previewMode: boolean;
+        timeformat: (value: any) => string;
+    }
+
+    type JsonViewerType = JsonViewerProps & VNodeProps & AllowedComponentProps & ComponentCustomProps;
+    const JsonViewer: Component<JsonViewerType>;
+    export { JsonViewer };
+    const def: { install: (app: App) => void };
+    export default def;
+}
