6f38c40bfc8fae5f40410f5fc1689d7e34b62c93
Stéphane TRÉBEL (WeScale) <stephane.trebel@wescale.fr>
2025-09-01 15:28:16 +0200

    feat(opencds): handle OpenCDS API token-based security

--- apps/server/src/resources/service-chain/queries.ts
+++ apps/server/src/resources/service-chain/queries.ts
@@ -1,62 +1,58 @@
 import {
+  type ServiceChain,
   ServiceChainDetailsSchema,
   ServiceChainFlowsSchema,
   ServiceChainListSchema,
-  type ServiceChain,
 } from '@cpn-console/shared'
 import axios from 'axios'
+import https from 'node:https'
 
 const openCDSEnvVar = 'OPENCDS_URL'
-const opencdsURL = process.env[openCDSEnvVar]
-const apiPrefix = 'api/v1'
-const openCDSDisabledErrorMessage = `OpenCDS is disabled, please set ${
-  openCDSEnvVar
-} in your relevant .env file. See .env-example`
+const openCDSTargetURL = process.env[openCDSEnvVar]
+const openCDSDisabledErrorMessage = `OpenCDS is disabled, please set ${openCDSEnvVar} in your relevant .env file. See .env-example`
 
-function getOpenCDSBaseURL() {
-  return `${opencdsURL}/${apiPrefix}`
+function getClient() {
+  if (!openCDSTargetURL) {
+    throw new Error(openCDSDisabledErrorMessage)
+  }
+  return axios.create({
+    baseURL: openCDSTargetURL,
+    httpsAgent: new https.Agent({
+      rejectUnauthorized:
+        // We want it to be `false` only if it has explicitly
+        // been stated as "false" in the env vars
+        process.env.OPENCDS_API_TLS_REJECT_UNAUTHORIZED !== 'false',
+    }),
+    headers: {
+      'X-API-Key': process.env.OPENCDS_API_TOKEN,
+    },
+  })
 }
 
 export async function listServiceChains() {
-  if (opencdsURL) {
-    return ServiceChainListSchema.parse(
-      (await axios.get(`${getOpenCDSBaseURL()}/requests`)).data,
-    )
-  }
-  throw new Error(openCDSDisabledErrorMessage)
+  return ServiceChainListSchema.parse(
+    (await getClient().get(`/requests`)).data,
+  )
 }
 
 export async function getServiceChainDetails(
   serviceChainId: ServiceChain['id'],
 ) {
-  if (opencdsURL) {
-    return ServiceChainDetailsSchema.parse((await axios.get(`${getOpenCDSBaseURL()}/requests/${serviceChainId}`)).data)
-  }
-  throw new Error(openCDSDisabledErrorMessage)
+  return ServiceChainDetailsSchema.parse(
+    (await getClient().get(`/requests/${serviceChainId}`)).data,
+  )
 }
 
 export async function retryServiceChain(serviceChainId: ServiceChain['id']) {
-  if (opencdsURL) {
-    return await axios.post(
-      `${getOpenCDSBaseURL()}/requests/${serviceChainId}/retry`,
-    )
-  }
-  throw new Error(openCDSDisabledErrorMessage)
+  return await getClient().post(`/requests/${serviceChainId}/retry`)
 }
 
 export async function validateServiceChain(validationId: string) {
-  if (opencdsURL) {
-    return await axios.post(`${getOpenCDSBaseURL()}/validate/${validationId}`)
-  }
-  throw new Error(openCDSDisabledErrorMessage)
+  return await getClient().post(`/validate/${validationId}`)
 }
 
-export async function getServiceChainFlows(
-  serviceChainId: ServiceChain['id'],
-) {
-  if (opencdsURL) {
-    const response = await axios.get(`${getOpenCDSBaseURL()}/requests/${serviceChainId}/flows`)
-    return ServiceChainFlowsSchema.parse((response).data)
-  }
-  throw new Error(openCDSDisabledErrorMessage)
+export async function getServiceChainFlows(serviceChainId: ServiceChain['id']) {
+  return ServiceChainFlowsSchema.parse(
+    (await getClient().get(`/requests/${serviceChainId}/flows`)).data,
+  )
 }
