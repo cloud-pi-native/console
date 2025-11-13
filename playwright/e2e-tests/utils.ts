8370b8b4adbe5c1b65bc5f45bd50c860beeba96c
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-11-13 16:29:35 +0100

    chore(e2e): migrate repository tests from Cypress to Playwright

--- playwright/e2e-tests/utils.ts
+++ playwright/e2e-tests/utils.ts
@@ -288,3 +288,8 @@ export async function removeEnvFromProject(
   await page.getByTestId('deleteEnvironmentBtn').click()
   await expect(page.getByTestId(`environmentTr-${envName}`)).not.toBeVisible()
 }
+
+export const invalidGitUrlErrorMessage = 'L\'adresse doit commencer par https et se terminer par .git'
+export const invalidInternalRepoErrorMessage = 'Le nom du dépôt ne doit contenir ni majuscules, ni espaces, ni caractères spéciaux hormis le trait d\'union, et doit commencer et se terminer par un caractère alphanumérique'
+export const fakeToken = 'fakeToken'
+export const deleteValidationInput = 'DELETE'
