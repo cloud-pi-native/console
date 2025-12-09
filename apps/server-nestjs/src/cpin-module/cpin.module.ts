5307afac77137cd42c527ad39dc2232cda3bf23b
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-12-09 16:50:07 +0100

    chore: add old-server new Nest.js services to CpinModule

--- apps/server-nestjs/src/cpin-module/cpin.module.ts
+++ apps/server-nestjs/src/cpin-module/cpin.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { AppService } from '@old-server/app';
+import { ConnectionService } from '@old-server/connect';
+import { PrepareAppService } from '@old-server/prepare-app';
+import { ResourcesService } from '@old-server/resources';
 import { ServerService } from '@old-server/server';
+import { FastifyService } from '@old-server/utils/fastify';
+import { LoggerService } from '@old-server/utils/logger';
 
 @Module({
     controllers: [],
-    providers: [ServerService],
+    providers: [
+        AppService,
+        ConnectionService,
+        FastifyService,
+        LoggerService,
+        PrepareAppService,
+        ResourcesService,
+        ServerService,
+    ],
 })
 export class CpinModule {}
