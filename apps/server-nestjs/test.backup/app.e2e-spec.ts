0dc0020a2825eb1957961089f6a65833703bdfc9
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-12-09 17:12:55 +0100

    chore: disable tests from nest.js for now

--- apps/server-nestjs/test.backup/app.e2e-spec.ts
+++ apps/server-nestjs/test.backup/app.e2e-spec.ts
@@ -3,14 +3,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { App } from 'supertest/types';
 
-import { AppModule } from './../src/app.module';
+import { MainModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication<App>;
 
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
-            imports: [AppModule],
+            imports: [MainModule],
         }).compile();
 
         app = moduleFixture.createNestApplication();
