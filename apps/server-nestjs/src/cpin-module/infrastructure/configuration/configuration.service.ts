8c850b72c8d6df779b605db18e5caa0ad81d279f
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-12-12 15:47:43 +0100

    chore(nest-js): add basic config and logger modules based on existing implementation

--- apps/server-nestjs/src/cpin-module/infrastructure/configuration/configuration.service.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/configuration/configuration.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ConfigurationService {}
+export class ConfigurationService {
+    // @TODO: Rework this with proper environment handling
+    public readonly environment = 'development';
+}
