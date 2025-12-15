73c27d93ab779155a7e2c1c353909f3e5338b818
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-12-15 15:39:00 +0100

    chore(server-nestjs): add database initialization code in DatabaseInitializationService

--- apps/server-nestjs/src/cpin-module/application-initialization/application-initialization.module.ts
+++ apps/server-nestjs/src/cpin-module/application-initialization/application-initialization.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { ConfigurationModule } from '../infrastructure/configuration/configuration.module';
 import { ApplicationInitializationService } from './application-initialization-service/application-initialization.service';
 import { DatabaseInitializationService } from './database-initialization/database-initialization.service';
 import { PluginManagementService } from './plugin-management/plugin-management.service';
 
 @Module({
+    imports: [ConfigurationModule],
     providers: [
         ApplicationInitializationService,
         DatabaseInitializationService,
