64a113d6591739557d459562acc57456fc3f74b9
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-12-15 15:35:18 +0100

    chore(server-nestjs): rework configuration service to properly instanciate logger service

--- apps/server-nestjs/src/cpin-module/infrastructure/configuration/configuration.module.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/configuration/configuration.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { ConfigurationService } from './configuration.service';
 
+const pathList: string[] = [];
+
+if (process.env.DOCKER !== 'true') {
+    pathList.push('.env');
+}
+
+if (process.env.INTEGRATION === 'true') {
+    pathList.push('.env.integ');
+}
+
 @Module({
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: pathList,
+        }),
+    ],
     providers: [ConfigurationService],
-  exports: [ConfigurationService]
+    exports: [ConfigurationService],
 })
 export class ConfigurationModule {}
