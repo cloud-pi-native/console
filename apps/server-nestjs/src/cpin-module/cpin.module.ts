91d9596765ced2a941f8e31c514e2b6c3969ef95
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-12-15 16:49:30 +0100

    chore(server-nestjs): initialize core module and its AppService

--- apps/server-nestjs/src/cpin-module/cpin.module.ts
+++ apps/server-nestjs/src/cpin-module/cpin.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { ApplicationInitializationModule } from './application-initialization/application-initialization.module';
+import { CoreModule } from './core/core.module';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
 
 // This module host the old "server code" of our backend.
@@ -9,6 +10,10 @@ import { InfrastructureModule } from './infrastructure/infrastructure.module';
 @Module({
     controllers: [],
     providers: [],
-    imports: [ApplicationInitializationModule, InfrastructureModule],
+    imports: [
+        ApplicationInitializationModule,
+        CoreModule,
+        InfrastructureModule,
+    ],
 })
 export class CpinModule {}
