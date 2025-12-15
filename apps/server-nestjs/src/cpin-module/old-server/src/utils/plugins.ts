2d3133d5203fc94e71d3e4a2dfb5d4fc4e6bd784
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-12-15 15:43:35 +0100

    chore(server-nestjs): add code to PluginManagementService

--- apps/server-nestjs/src/cpin-module/old-server/src/utils/plugins.ts
+++ apps/server-nestjs/src/cpin-module/old-server/src/utils/plugins.ts
@@ -1,9 +1,9 @@
-import type { PluginManagerOptions } from '@cpn-console/hooks'
-import { isCI, isInt, isProd } from './env'
+// import type { PluginManagerOptions } from '@cpn-console/hooks'
+// import { isCI, isInt, isProd } from './env'
 
-export const pluginManagerOptions: PluginManagerOptions = {
-  mockHooks: isCI || (!isProd && !isInt),
-  mockMonitoring: isCI || (!isProd && !isInt),
-  mockExternalServices: isCI || (!isProd && !isInt),
-  startPlugins: (!isCI && isProd) || isInt,
-}
+// export const pluginManagerOptions: PluginManagerOptions = {
+  // mockHooks: isCI || (!isProd && !isInt),
+  // mockMonitoring: isCI || (!isProd && !isInt),
+  // mockExternalServices: isCI || (!isProd && !isInt),
+  // startPlugins: (!isCI && isProd) || isInt,
+// }
