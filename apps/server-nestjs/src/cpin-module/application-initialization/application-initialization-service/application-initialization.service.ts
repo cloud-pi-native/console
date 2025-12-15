ce17205780c517af7b9d2c7cf0ffc741d08e3bfe
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-12-15 15:39:00 +0100

    chore(server-nestjs): add database initialization code in DatabaseInitializationService

--- apps/server-nestjs/src/cpin-module/application-initialization/application-initialization-service/application-initialization.service.ts
+++ apps/server-nestjs/src/cpin-module/application-initialization/application-initialization-service/application-initialization.service.ts
@@ -1,4 +1,125 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { rm } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { ConfigurationService } from 'src/cpin-module/infrastructure/configuration/configuration.service';
+
+import app from './app';
+import { getConnection } from './connect';
+import { initDb } from './init/db/index';
+import { initPm } from './plugins';
 
 @Injectable()
-export class ApplicationInitializationService {}
+export class ApplicationInitializationService {
+    private readonly logger = new Logger(ApplicationInitializationService.name);
+    constructor(private readonly config: ConfigurationService) {}
+
+    async setUpHTTPProxy() {
+        // Workaround because fetch isn't using http_proxy variables
+        // See. https://github.com/gajus/global-agent/issues/52#issuecomment-1134525621
+        if (process.env.HTTP_PROXY) {
+            const Undici = await import('undici');
+            const ProxyAgent = Undici.ProxyAgent;
+            const setGlobalDispatcher = Undici.setGlobalDispatcher;
+            setGlobalDispatcher(new ProxyAgent(process.env.HTTP_PROXY));
+        }
+    }
+
+    async injectDataInDatabase(path: string) {
+        this.logger.log('Starting init DB...');
+        const { data } = await import(path);
+        await initDb(data);
+        this.logger.log('initDb invoked successfully');
+    }
+
+    async startServer() {
+        try {
+            await getConnection();
+        } catch (error) {
+            if (!(error instanceof Error)) return;
+            this.logger.error(error.message);
+            throw error;
+        }
+
+        initPm();
+
+        this.logger.log('Reading init database file');
+
+        try {
+            const dataPath =
+                this.config.isProd || this.config.isInt
+                    ? './init/db/imports/data'
+                    : '@cpn-console/test-utils/src/imports/data';
+            await this.injectDataInDatabase(dataPath);
+            if (this.config.isProd && !this.config.isDevSetup) {
+                this.logger.log('Cleaning up imported data file...');
+                await rm(resolve(__dirname, dataPath));
+                this.logger.log(`Successfully deleted '${dataPath}'`);
+            }
+        } catch (error) {
+            if (
+                error.code === 'ERR_MODULE_NOT_FOUND' ||
+                error.message.includes('Failed to load') ||
+                error.message.includes('Cannot find module')
+            ) {
+                this.logger.log('No initDb file, skipping');
+            } else {
+                this.logger.warn(error.message);
+                throw error;
+            }
+        }
+
+        this.logger.debug({
+            isDev: this.config.isDev,
+            isTest: this.config.isTest,
+            isCI: this.config.isCI,
+            isDevSetup: this.config.isDevSetup,
+            isProd: this.config.isProd,
+        });
+    }
+
+    async getPreparedApp() {
+        try {
+            await getConnection();
+        } catch (error) {
+            this.logger.error(error.message);
+            throw error;
+        }
+
+        initPm();
+
+        this.logger.log('Reading init database file');
+
+        try {
+            const dataPath =
+                this.config.isProd || this.config.isInt
+                    ? './init/db/imports/data'
+                    : '@cpn-console/test-utils/src/imports/data';
+            await this.injectDataInDatabase(dataPath);
+            if (this.config.isProd && !this.config.isDevSetup) {
+                this.logger.log('Cleaning up imported data file...');
+                await rm(resolve(__dirname, dataPath));
+                this.logger.log(`Successfully deleted '${dataPath}'`);
+            }
+        } catch (error) {
+            if (
+                error.code === 'ERR_MODULE_NOT_FOUND' ||
+                error.message.includes('Failed to load') ||
+                error.message.includes('Cannot find module')
+            ) {
+                this.logger.log('No initDb file, skipping');
+            } else {
+                this.logger.warn(error.message);
+                throw error;
+            }
+        }
+
+        this.logger.debug({
+            isDev: this.config.isDev,
+            isTest: this.config.isTest,
+            isCI: this.config.isCI,
+            isDevSetup: this.config.isDevSetup,
+            isProd: this.config.isProd,
+        });
+        return app;
+    }
+}
--- apps/server-nestjs/src/cpin-module/application-initialization/database-initialization/database-initialization.service.ts
+++ apps/server-nestjs/src/cpin-module/application-initialization/database-initialization/database-initialization.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import prisma from '@/prisma';
+import { Injectable, Logger } from '@nestjs/common';
+
+import { modelKeys } from './utils';
+
+type ExtractKeysWithFields<T> = {
+    [K in keyof T]: T[K] extends { fields: any } ? K : never;
+}[keyof T];
+
+type Models = ExtractKeysWithFields<typeof prisma>;
+
+type Imports = Partial<Record<Models, object[]>> & {
+    associations: [Models, any[]];
+};
 
 @Injectable()
-export class DatabaseInitializationService {}
+export class DatabaseInitializationService {
+    private readonly loggerService = new Logger(
+        DatabaseInitializationService.name,
+    );
+
+    async initDb(data: Imports) {
+        const dataStringified = JSON.stringify(data);
+        const dataParsed = JSON.parse(dataStringified, (key, value) => {
+            try {
+                if (['permissions', 'everyonePerms'].includes(key)) {
+                    return BigInt(value.slice(0, value.length - 1));
+                }
+            } catch (_error) {
+                return value;
+            }
+            return value;
+        });
+        this.loggerService.log('Drop tables');
+        for (const modelKey of modelKeys.toReversed()) {
+            // @ts-ignore
+            await prisma[modelKey].deleteMany();
+        }
+        this.loggerService.log('Import models');
+        for (const modelKey of modelKeys) {
+            // @ts-ignore
+            await prisma[modelKey].createMany({ data: dataParsed[modelKey] });
+        }
+        this.loggerService.log('Import associations');
+        for (const [modelKey, rows] of dataParsed.associations) {
+            for (const row of rows) {
+                const idKey = 'id';
+                const connectKeys = Object.keys(row).filter(
+                    (key) => key !== idKey,
+                );
+                const dataConnects = connectKeys.reduce(
+                    (acc, curr) => {
+                        acc[curr] = { connect: row[curr] };
+                        return acc;
+                    },
+                    {} as Record<string, { connect: any[] }>,
+                );
+                // @ts-ignore
+                await prisma[modelKey].update({
+                    where: { id: row.id },
+                    data: dataConnects,
+                });
+            }
+        }
+        this.loggerService.log('End import');
+    }
+}
