54ccd108694ed45e0fa48b1225582db4bc0ecc4d
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-12-22 15:58:56 +0100

    fix(shared): fix a rare pnpm vs typescript bug
    Here is a stellar explanation of this issue:
    https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1519138189

--- packages/shared/src/api-client.ts
+++ packages/shared/src/api-client.ts
@@ -6,32 +6,38 @@ export const apiPrefix: string = '/api/v1'
 export const contractInstance: ReturnType<typeof initContract> = initContract()
 
 export async function getContract() {
-  return contractInstance.router(
-    {
-      AdminTokens: (await import('./contracts/index.js')).adminTokenContract,
-      AdminRoles: (await import('./contracts/index.js')).adminRoleContract,
-      Clusters: (await import('./contracts/index.js')).clusterContract,
-      ServiceChains: (await import('./contracts/index.js')).serviceChainContract,
-      Environments: (await import('./contracts/index.js')).environmentContract,
-      Logs: (await import('./contracts/index.js')).logContract,
-      PersonalAccessTokens: (await import('./contracts/index.js')).personalAccessTokenContract,
-      Projects: (await import('./contracts/index.js')).projectContract,
-      ProjectsMembers: (await import('./contracts/index.js')).projectMemberContract,
-      ProjectsRoles: (await import('./contracts/index.js')).projectRoleContract,
-      ProjectServices: (await import('./contracts/index.js')).projectServiceContract,
-      Repositories: (await import('./contracts/index.js')).repositoryContract,
-      Stages: (await import('./contracts/index.js')).stageContract,
-      Services: (await import('./contracts/index.js')).serviceContract,
-      Users: (await import('./contracts/index.js')).userContract,
-      Zones: (await import('./contracts/index.js')).zoneContract,
-      System: (await import('./contracts/index.js')).systemContract,
-      SystemPlugin: (await import('./contracts/index.js')).systemPluginContract,
-      SystemSettings: (await import('./contracts/index.js')).systemSettingsContract,
-    },
-  )
+  return contractInstance.router({
+    AdminTokens: (await import('./contracts/index.js')).adminTokenContract,
+    AdminRoles: (await import('./contracts/index.js')).adminRoleContract,
+    Clusters: (await import('./contracts/index.js')).clusterContract,
+    ServiceChains: (await import('./contracts/index.js')).serviceChainContract,
+    Environments: (await import('./contracts/index.js')).environmentContract,
+    Logs: (await import('./contracts/index.js')).logContract,
+    PersonalAccessTokens: (await import('./contracts/index.js'))
+      .personalAccessTokenContract,
+    Projects: (await import('./contracts/index.js')).projectContract,
+    ProjectsMembers: (await import('./contracts/index.js'))
+      .projectMemberContract,
+    ProjectsRoles: (await import('./contracts/index.js')).projectRoleContract,
+    ProjectServices: (await import('./contracts/index.js'))
+      .projectServiceContract,
+    Repositories: (await import('./contracts/index.js')).repositoryContract,
+    Stages: (await import('./contracts/index.js')).stageContract,
+    Services: (await import('./contracts/index.js')).serviceContract,
+    Users: (await import('./contracts/index.js')).userContract,
+    Zones: (await import('./contracts/index.js')).zoneContract,
+    System: (await import('./contracts/index.js')).systemContract,
+    SystemPlugin: (await import('./contracts/index.js')).systemPluginContract,
+    SystemSettings: (await import('./contracts/index.js'))
+      .systemSettingsContract,
+  })
 }
 
-export async function getApiClient(baseUrl: string, baseHeaders: Record<string, string>, api: ApiFetcher | undefined) {
+export async function getApiClient(
+  baseUrl: string,
+  baseHeaders: Record<string, string>,
+  api: ApiFetcher | undefined,
+): Promise<any> {
   return initClient(await getContract(), {
     baseUrl,
     baseHeaders,
