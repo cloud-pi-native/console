bbe07b27bfd3d6c198e17a642cc441c919392d85
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-12-22 14:30:30 +0100

    chore(server-nestjs): delete unneeded code

--- apps/server-nestjs/src/cpin-module/infrastructure/infrastructure.module.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/infrastructure.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 
 import { ConfigurationModule } from './configuration/configuration.module';
 import { DatabaseService } from './database/database.service';
-import { FastifyService } from './fastify/fastify.service';
 import { HttpClientService } from './http-client/http-client.service';
 import { LoggerModule } from './logger/logger.module';
 import { ServerService } from './server/server.service';
@@ -11,14 +10,12 @@ import { ServerService } from './server/server.service';
     providers: [
         DatabaseService,
         HttpClientService,
-        FastifyService,
         ServerService,
     ],
     imports: [LoggerModule, ConfigurationModule],
     exports: [
         DatabaseService,
         HttpClientService,
-        FastifyService,
         ServerService,
     ],
 })
