0d8c958c3fb5f17671ab3ef20825cdaaca47581a
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-21 10:19:41 +0100

    fix: prisma not loading dotenv
    When running outside of apps/server as per the readme, prisma migrate will fails.
    
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server/prisma.config.ts
+++ apps/server/prisma.config.ts
@@ -1,5 +1,18 @@
 import path from 'node:path'
 import { defineConfig } from 'prisma/config'
+import * as dotenv from 'dotenv'
+
+if (process.env.DOCKER !== 'true') {
+  dotenv.config({ path: '.env' })
+}
+
+if (process.env.INTEGRATION === 'true') {
+  const envInteg = dotenv.config({ path: '.env.integ' })
+  process.env = {
+    ...process.env,
+    ...(envInteg?.parsed ?? {}),
+  }
+}
 
 export default defineConfig({
   schema: path.join('src', 'prisma', 'schema'),
