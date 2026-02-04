b780db0b2aad431bd0dc4035a01a546e0e2c3648
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2026-02-04 10:42:05 +0100

    chore: remove all local kubernetes cluster code
    We don't use it, we don't have the resources to maintain it, and
    everything is in the helm-chart repo anyway.

--- ci/kind/upload-package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-dir=$(find packages plugins -mindepth 1 -maxdepth 2 -type d -name "$1" | tail -n 1)
-packageName=$(jq -r '.name' "${dir}/package.json")
-podName=$(kubectl --context kind-kind get pods -o name | grep server | cut -d "/" -f2)
-kubectl --context kind-kind cp "${dir}/dist/" "${podName}:/app/apps/server/node_modules/${packageName}"
-echo "$packageName uploaded"
