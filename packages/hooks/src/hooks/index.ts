fa20c0e7f4fc90cd6299d814d59696feb389b1c7
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-02-05 18:06:36 +0100

    feat(keycloak): propagate admin roles to Keycloack
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- packages/hooks/src/hooks/index.ts
+++ packages/hooks/src/hooks/index.ts
@@ -4,6 +4,7 @@ export * from './hook-project.js'
 export * from './hook-project-role.js'
 export * from './hook-user.js'
 export * from './hook-zone.js'
+export * from './hook-admin-role.js'
 
 export interface PartialEnvironment { environment: string, stage: string, clusterLabel: string }
 export type InternalRepoName = string
