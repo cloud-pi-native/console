4ced1e8c6ab1ab6fbdbac483025e7e38e3c66546
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-02-06 15:43:04 +0100

    fix(admin-roles): admin conflicting with existing admin group in Keycloack
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server/src/prisma/migrations/20260204150335_add_system_roles/migration.sql
+++ apps/server/src/prisma/migrations/20260204150335_add_system_roles/migration.sql
@@ -1,10 +1,10 @@
 -- Update existing Admin role to be system role 'Administrateur Plateforme'
 UPDATE "AdminRole"
-SET 
+SET
   "name" = 'Administrateur Plateforme',
   "type" = 'system',
   "permissions" = 3, -- Assuming 3n means bit 0 and 1 (1 | 2 = 3)
-  "oidcGroup" = '/admin',
+  "oidcGroup" = '/console/admin',
   "position" = 0
 WHERE "id" = '76229c96-4716-45bc-99da-00498ec9018c'::uuid;
 
@@ -15,7 +15,7 @@ VALUES (
   'Lecture Seule Plateforme',
   1, -- Assuming 1n means bit 0
   2,
-  '/readonly',
+  '/console/readonly',
   'system'
 )
 ON CONFLICT ("id") DO UPDATE
@@ -23,4 +23,4 @@ SET
   "name" = 'Lecture Seule Plateforme',
   "type" = 'system',
   "permissions" = 1,
-  "oidcGroup" = '/readonly';
+  "oidcGroup" = '/console/readonly';
