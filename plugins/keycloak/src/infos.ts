6d89d1792e2c29e4aed0fe8e0fced5f1e31b7222
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-02-09 14:57:54 +0100

    feat(keycloack): add configurable purge at upsert
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- plugins/keycloak/src/infos.ts
+++ plugins/keycloak/src/infos.ts
@@ -1,8 +1,27 @@
 import type { ServiceInfos } from '@cpn-console/hooks'
+import { DISABLED } from '@cpn-console/shared'
 
 const infos: ServiceInfos = {
   name: 'keycloak',
   title: 'Keycloak',
+  config: {
+    global: [
+    ],
+    project: [
+      {
+        kind: 'switch',
+        key: 'purge',
+        initialValue: DISABLED,
+        permissions: {
+          admin: { read: true, write: true },
+          user: { read: false, write: false },
+        },
+        title: 'Purger les utilisateurs non synchronisés',
+        value: DISABLED,
+        description: 'Purger les utilisateurs non synchronisés de Keycloak lors de la synchronisation',
+      },
+    ],
+  },
 }
 
 export default infos
