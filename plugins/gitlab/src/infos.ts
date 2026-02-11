f105cbefe636e89b0f162e6c8afb9eb1775e8064
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-02-11 11:16:35 +0100

    feat(project-roles): add projects roles to GitLab groups
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- plugins/gitlab/src/infos.ts
+++ plugins/gitlab/src/infos.ts
@@ -1,6 +1,12 @@
 import type { ServiceInfos } from '@cpn-console/hooks'
-import { ENABLED } from '@cpn-console/shared'
+import { DISABLED, ENABLED } from '@cpn-console/shared'
 import config from './config.js'
+
+export const DEFAULT_ADMIN_GROUP_PATH = '/console/admin'
+export const DEFAULT_AUDITOR_GROUP_PATH = '/console/readonly'
+export const DEFAULT_PROJECT_MAINTAINER_GROUP_PATH_SUFFIX = '/console/admin'
+export const DEFAULT_PROJECT_DEVELOPER_GROUP_PATH_SUFFIX = '/console/developer'
+export const DEFAULT_PROJECT_REPORTER_GROUP_PATH_SUFFIX = '/console/readonly'
 
 const infos = {
   name: 'gitlab',
@@ -9,40 +15,92 @@ const infos = {
   imgSrc: '/img/gitlab.svg',
   description: 'GitLab est un service d\'hébergement de code source et de pipeline CI/CD',
   config: {
-    global: [{
-      kind: 'switch',
-      key: 'displayTriggerHint',
-      initialValue: ENABLED,
-      permissions: {
-        admin: { read: true, write: true },
-        user: { read: false, write: false },
+    global: [
+      {
+        kind: 'switch',
+        key: 'purge',
+        initialValue: DISABLED,
+        permissions: {
+          admin: { read: true, write: true },
+          user: { read: false, write: false },
+        },
+        title: 'Purger les utilisateurs non synchronisés',
+        value: DISABLED,
+        description: 'Purger les utilisateurs non synchronisés de GitLab lors de la synchronisation',
       },
-      title: 'Afficher l\'aide de trigger de pipeline',
-      value: ENABLED,
-      description: 'Afficher l\'aide de trigger de pipeline aux utilisateurs lorsqu\'ils souhaitent afficher les secrets du projet',
-    }, {
-      kind: 'text',
-      key: 'adminGroupPath',
-      permissions: {
-        admin: { read: true, write: true },
-        user: { read: false, write: false },
+      {
+        kind: 'switch',
+        key: 'displayTriggerHint',
+        initialValue: ENABLED,
+        permissions: {
+          admin: { read: true, write: true },
+          user: { read: false, write: false },
+        },
+        title: 'Afficher l\'aide de trigger de pipeline',
+        value: ENABLED,
+        description: 'Afficher l\'aide de trigger de pipeline aux utilisateurs lorsqu\'ils souhaitent afficher les secrets du projet',
       },
-      title: 'Chemin du groupe OIDC Admin',
-      value: '/console/admin',
-      description: 'Le chemin du groupe OIDC qui donne les droits d\'administrateur GitLab',
-      placeholder: '/console/admin',
-    }, {
-      kind: 'text',
-      key: 'auditorGroupPath',
-      permissions: {
-        admin: { read: true, write: true },
-        user: { read: false, write: false },
+      {
+        kind: 'text',
+        key: 'adminGroupPath',
+        permissions: {
+          admin: { read: true, write: true },
+          user: { read: false, write: false },
+        },
+        title: 'Chemin du groupe OIDC Admin',
+        value: DEFAULT_ADMIN_GROUP_PATH,
+        description: 'Le chemin du groupe OIDC qui donne les droits d\'administrateur GitLab',
+        placeholder: DEFAULT_ADMIN_GROUP_PATH,
       },
-      title: 'Chemin du groupe OIDC Auditeur',
-      value: '/console/readonly',
-      description: 'Le chemin du groupe OIDC qui donne les droits d\'auditeur GitLab',
-      placeholder: '/console/readonly',
-    }],
+      {
+        kind: 'text',
+        key: 'auditorGroupPath',
+        permissions: {
+          admin: { read: true, write: true },
+          user: { read: false, write: false },
+        },
+        title: 'Chemin du groupe OIDC Auditeur',
+        value: DEFAULT_AUDITOR_GROUP_PATH,
+        description: 'Le chemin du groupe OIDC qui donne les droits d\'auditeur GitLab',
+        placeholder: DEFAULT_AUDITOR_GROUP_PATH,
+      },
+      {
+        kind: 'text',
+        key: 'projectMaintainerGroupPathSuffix',
+        permissions: {
+          admin: { read: true, write: true },
+          user: { read: false, write: false },
+        },
+        title: 'Suffixe du chemin du groupe OIDC Maintainer',
+        value: DEFAULT_PROJECT_MAINTAINER_GROUP_PATH_SUFFIX,
+        description: 'Suffixe du groupe OIDC donnant accès Maintainer',
+        placeholder: DEFAULT_PROJECT_MAINTAINER_GROUP_PATH_SUFFIX,
+      },
+      {
+        kind: 'text',
+        key: 'projectDeveloperGroupPathSuffix',
+        permissions: {
+          admin: { read: true, write: true },
+          user: { read: false, write: false },
+        },
+        title: 'Suffixe du chemin du groupe OIDC Developer',
+        value: DEFAULT_PROJECT_DEVELOPER_GROUP_PATH_SUFFIX,
+        description: 'Suffixe du groupe OIDC donnant accès Developer',
+        placeholder: DEFAULT_PROJECT_DEVELOPER_GROUP_PATH_SUFFIX,
+      },
+      {
+        kind: 'text',
+        key: 'projectReporterGroupPathSuffix',
+        permissions: {
+          admin: { read: true, write: true },
+          user: { read: false, write: false },
+        },
+        title: 'Suffixe du chemin du groupe OIDC Reporter',
+        value: DEFAULT_PROJECT_REPORTER_GROUP_PATH_SUFFIX,
+        description: 'Suffixe du groupe OIDC donnant accès Reporter',
+        placeholder: DEFAULT_PROJECT_REPORTER_GROUP_PATH_SUFFIX,
+      },
+    ],
     project: [],
   },
 } as const satisfies ServiceInfos
