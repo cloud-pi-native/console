025a6811059d9607e7b355c1f72f5abecd9d8aec
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-02-16 18:00:12 +0100

    feat(shared): add admin role fine grained permissions
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/cpin-module/infrastructure/infrastructure.module.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/infrastructure.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
 
-import { ConfigurationModule } from './configuration/configuration.module';
-import { DatabaseService } from './database/database.service';
-import { HttpClientService } from './http-client/http-client.service';
-import { LoggerModule } from './logger/logger.module';
-import { ServerService } from './server/server.service';
+import { ConfigurationModule } from './configuration/configuration.module'
+import { DatabaseService } from './database/database.service'
+import { HttpClientService } from './http-client/http-client.service'
+import { LoggerModule } from './logger/logger.module'
+import { ServerService } from './server/server.service'
 
 @Module({
-    providers: [DatabaseService, HttpClientService, ServerService],
-    imports: [LoggerModule, ConfigurationModule],
-    exports: [DatabaseService, HttpClientService, ServerService],
+  providers: [DatabaseService, HttpClientService, ServerService],
+  imports: [LoggerModule, ConfigurationModule],
+  exports: [DatabaseService, HttpClientService, ServerService],
 })
 export class InfrastructureModule {}
