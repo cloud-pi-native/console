c9eb1de8b99a349fbf1701fd7317583d71873c18
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2026-02-19 10:11:51 +0100

    chore(server-nestjs): align formatting rules on other packages
    This means we are getting rid of prettier here, for now.
    Consistency above all.

--- apps/server-nestjs/src/cpin-module/infrastructure/database/database.service.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/database/database.service.ts
@@ -1,70 +1,70 @@
-import prisma from '@/prisma';
-import { Injectable, Logger } from '@nestjs/common';
-import { setTimeout } from 'node:timers/promises';
+import prisma from '@/prisma'
+import { Injectable, Logger } from '@nestjs/common'
+import { setTimeout } from 'node:timers/promises'
 
-import { ConfigurationService } from '../configuration/configuration.service';
+import type { ConfigurationService } from '../configuration/configuration.service'
 
 @Injectable()
 export class DatabaseService {
-    private readonly loggerService = new Logger(DatabaseService.name);
+  private readonly loggerService = new Logger(DatabaseService.name)
 
-    constructor(private readonly configurationService: ConfigurationService) {
-        this.DELAY_BEFORE_RETRY =
-            this.configurationService.isTest || this.configurationService.isCI
-                ? 1000
-                : 10000;
+  constructor(private readonly configurationService: ConfigurationService) {
+    this.DELAY_BEFORE_RETRY
+            = this.configurationService.isTest || this.configurationService.isCI
+        ? 1000
+        : 10000
+  }
+
+  DELAY_BEFORE_RETRY!: number
+  closingConnections = false
+
+  async getConnection(triesLeft = 5): Promise<void> {
+    if (this.closingConnections || triesLeft <= 0) {
+      throw new Error('Unable to connect to Postgres server')
     }
+    triesLeft--
 
-    DELAY_BEFORE_RETRY!: number;
-    closingConnections = false;
+    try {
+      if (
+        this.configurationService.isDev
+        || this.configurationService.isTest
+        || this.configurationService.isCI
+      ) {
+        this.loggerService.log(
+          `Trying to connect to Postgres with: ${this.configurationService.dbUrl}`,
+        )
+      }
+      await prisma.$connect()
 
-    async getConnection(triesLeft = 5): Promise<void> {
-        if (this.closingConnections || triesLeft <= 0) {
-            throw new Error('Unable to connect to Postgres server');
-        }
-        triesLeft--;
+      this.loggerService.log('Connected to Postgres!')
+    } catch (error) {
+      if (triesLeft > 0) {
+        this.loggerService.error(error)
+        this.loggerService.log(
+          `Could not connect to Postgres: ${error.message}`,
+        )
+        this.loggerService.log(`Retrying (${triesLeft} tries left)`)
+        await setTimeout(this.DELAY_BEFORE_RETRY)
+        return this.getConnection(triesLeft)
+      }
 
-        try {
-            if (
-                this.configurationService.isDev ||
-                this.configurationService.isTest ||
-                this.configurationService.isCI
-            ) {
-                this.loggerService.log(
-                    `Trying to connect to Postgres with: ${this.configurationService.dbUrl}`,
-                );
-            }
-            await prisma.$connect();
-
-            this.loggerService.log('Connected to Postgres!');
-        } catch (error) {
-            if (triesLeft > 0) {
-                this.loggerService.error(error);
-                this.loggerService.log(
-                    `Could not connect to Postgres: ${error.message}`,
-                );
-                this.loggerService.log(`Retrying (${triesLeft} tries left)`);
-                await setTimeout(this.DELAY_BEFORE_RETRY);
-                return this.getConnection(triesLeft);
-            }
-
-            this.loggerService.log(
-                `Could not connect to Postgres: ${error.message}`,
-            );
-            this.loggerService.log('Out of retries');
-            error.message = `Out of retries, last error: ${error.message}`;
-            throw error;
-        }
+      this.loggerService.log(
+        `Could not connect to Postgres: ${error.message}`,
+      )
+      this.loggerService.log('Out of retries')
+      error.message = `Out of retries, last error: ${error.message}`
+      throw error
     }
+  }
 
-    async closeConnections() {
-        this.closingConnections = true;
-        try {
-            await prisma.$disconnect();
-        } catch (error) {
-            this.loggerService.error(error);
-        } finally {
-            this.closingConnections = false;
-        }
+  async closeConnections() {
+    this.closingConnections = true
+    try {
+      await prisma.$disconnect()
+    } catch (error) {
+      this.loggerService.error(error)
+    } finally {
+      this.closingConnections = false
     }
+  }
 }
--- apps/server-nestjs/src/main.module.ts
+++ apps/server-nestjs/src/main.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
 
-import { CpinModule } from './cpin-module/cpin.module';
+import { CpinModule } from './cpin-module/cpin.module'
 
 // This module only exists to import other module.
 // « One module to rule them all, and in NestJs bind them »
 @Module({
-    imports: [CpinModule],
-    controllers: [],
-    providers: [],
+  imports: [CpinModule],
+  controllers: [],
+  providers: [],
 })
 export class MainModule {}
