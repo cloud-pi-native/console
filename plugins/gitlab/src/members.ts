ad6b4b46a24490ef7c1733f8fd3add1f0c20f6eb
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-02-24 12:26:29 +0100

    feat(gitlab): add builtin roles to GitLab
    Co-authered-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- plugins/gitlab/src/members.ts
+++ plugins/gitlab/src/members.ts
@@ -1,13 +1,10 @@
-import type { Project, Config } from '@cpn-console/hooks'
-import type { UserSchema } from '@gitbeaker/core'
-import { specificallyEnabled } from '@cpn-console/hooks'
+import type { Project } from '@cpn-console/hooks'
+import { AccessLevel, type UserSchema } from '@gitbeaker/core'
 import type { GitlabProjectApi } from './class.js'
 import { upsertUser } from './user.js'
-import { resolveAccessLevel } from './utils.js'
 
-export async function ensureMembers(gitlabApi: GitlabProjectApi, project: Project, config: Config) {
-  const purge = config.gitlab?.purge
-
+export async function ensureMembers(gitlabApi: GitlabProjectApi, project: Project) {
+  // Ensure all users exists in gitlab
   const [gitlabUserPromiseResults, members] = await Promise.all([
     Promise.allSettled(project.users.map(user => upsertUser(user))),
     gitlabApi.getGroupMembers(),
@@ -22,42 +19,26 @@ export async function ensureMembers(gitlabApi: GitlabProjectApi, project: Projec
   const rejectedGitlabUsers = gitlabUserPromiseResults
     .filter<RejectedResult>((result): result is RejectedResult => result.status === 'rejected')
 
-  const userAccessLevels = new Map<string, number>()
-  for (const role of project.roles) {
-    const accessLevel = resolveAccessLevel(project, role, config)
-
-    if (accessLevel !== undefined) {
-      for (const user of role.users) {
-        const currentLevel = userAccessLevels.get(user.id)
-        if (!currentLevel || accessLevel > currentLevel) {
-          userAccessLevels.set(user.id, accessLevel)
-        }
-      }
-    }
-  }
-
-  await Promise.all(fulfilledGitlabUsers.map(async (gitlabUser, index) => {
-    const projectUser = project.users[index]
-    if (!projectUser) return
-
-    const accessLevel = userAccessLevels.get(projectUser.id)
-    const existingMember = members.find(member => member.id === gitlabUser.value.id)
-
-    if (accessLevel !== undefined) {
-      if (existingMember) {
-        if (existingMember.access_level !== accessLevel) {
-          await gitlabApi.editGroupMember(gitlabUser.value.id, accessLevel)
-        }
-      } else {
-        await gitlabApi.addGroupMember(gitlabUser.value.id, accessLevel)
-      }
-    } else if (specificallyEnabled(purge) && existingMember) {
-      await gitlabApi.removeGroupMember(gitlabUser.value.id)
-    }
-  }))
-
+  // Ensure members are set
+  const membersAdded = await Promise.all([
+    ...fulfilledGitlabUsers.map((gitlabUser) => {
+      const user = project.users.find(user => user.email === gitlabUser.value.email)
+      const isOwner = project.roles.find(role => role.userId === user?.id)?.role === 'owner'
+      return members.find(member => member.id === gitlabUser.value.id)
+        ? undefined
+        : gitlabApi.addGroupMember(gitlabUser.value.id, isOwner ? AccessLevel.OWNER : AccessLevel.DEVELOPER)
+    }),
+    ...members.map(member =>
+      (
+        !member.username.match(/group_\d+_bot/)
+        && !fulfilledGitlabUsers.find(gitlabUser => member.id === gitlabUser.value.id)
+      )
+        ? gitlabApi.removeGroupMember(member.id)
+        : undefined,
+    ),
+  ])
   return {
-    members,
+    members: [...members, membersAdded.filter(member => member)],
     failedInUpsertUsers: !!rejectedGitlabUsers.length,
   }
 }
