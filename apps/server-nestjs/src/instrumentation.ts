fca4769df0d86e383ff092724be17bda8772ed43
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-04 16:02:14 +0100

    chore: add instrumentation Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/instrumentation.ts
@@ -0,0 +1,26 @@
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node'
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+import {
+  PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+} from '@opentelemetry/sdk-metrics'
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core'
+
+function createSdk() {
+  return new NodeSDK({
+    traceExporter: new ConsoleSpanExporter(),
+    metricReader: new PeriodicExportingMetricReader({
+      exporter: new ConsoleMetricExporter(),
+    }),
+    instrumentations: [getNodeAutoInstrumentations(), new NestInstrumentation()],
+  })
+}
+
+export function instrument() {
+  const sdk = createSdk()
+  sdk.start()
+  process.on('SIGTERM', () => {
+    sdk.shutdown()
+  })
+}
