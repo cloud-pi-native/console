73c59d23faeb74b0e19ae86527fd7536a24926ff
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-09 14:21:30 +0100

    refactor(keycloak): migrate Keycloak plugin to NestJS
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/test/app.e2e-spec.ts
+++ apps/server-nestjs/test/app.e2e-spec.ts
@@ -2,15 +2,26 @@ import type { INestApplication } from '@nestjs/common'
 import type { TestingModule } from '@nestjs/testing'
 import { Test } from '@nestjs/testing'
 import type { App } from 'supertest/types'
+import { vi } from 'vitest'
 
 import { MainModule } from './../src/main.module'
+import { PrismaService } from '@/cpin-module/infrastructure/database/prisma.service'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [MainModule],
+      providers: [
+        MainModule,
+        {
+          provide: PrismaService,
+          useValue: {
+            onModuleInit: vi.fn(),
+            onModuleDestroy: vi.fn(),
+          } satisfies Partial<PrismaService>,
+        },
+      ],
     }).compile()
 
     app = moduleFixture.createNestApplication()
