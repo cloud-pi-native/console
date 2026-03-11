ec269e3626d7f9785ff1f8d212ebbe4854e3feb5
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-11 18:22:20 +0100

    chore(vault): migrate Vault plugin to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/modules/vault/vault.service.ts
+++ apps/server-nestjs/src/modules/vault/vault.service.ts
@@ -2,6 +2,8 @@ import { Inject, Injectable, Logger } from '@nestjs/common'
 import { ConfigurationService } from '@/cpin-module/infrastructure/configuration/configuration.service'
 import { VaultClientService } from './vault-client.service'
 import type { VaultSecret } from './vault-client.service'
+import { generateAppAdminPolicyName, generateTechnicalReadOnlyPolicyName, generateZoneName } from './vault.utils'
+import type { ProjectWithDetails } from './vault-datastore.service'
 
 @Injectable()
 export class VaultService {
@@ -52,5 +54,56 @@ export class VaultService {
 
   async writeMirrorTriggerToken(secret: Record<string, any>) {
     return this.write(secret, 'GITLAB')
+  }
+
+  async upsertMount(kvName: string) {
+    const body = {
+      type: 'kv',
+      config: {
+        force_no_cache: true,
+      },
+      options: {
+        version: 2,
+      },
+    }
+    try {
+      await this.vaultClientService.updateMount(kvName, body)
+    } catch (_error) {
+      await this.vaultClientService.createMount(kvName, body)
+    }
+  }
+
+  async createZone(project: ProjectWithDetails, environment: ProjectWithDetails['environments'][number]) {
+    const kvName = generateZoneName(environment.name)
+    await this.upsertMount(kvName)
+    const techName = generateTechnicalReadOnlyPolicyName(project)
+    const appName = generateAppAdminPolicyName(project)
+    await Promise.all([
+      this.createTechnicalReadOnlyPolicy(techName, project.slug),
+      this.createAppAdminPolicy(appName, project.slug),
+    ])
+    await this.vaultClientService.upsertRole(kvName, [
+      techName,
+      appName,
+    ])
+  }
+
+  async createTechnicalReadOnlyPolicy(name: string, projectSlug: string) {
+    await this.vaultClientService.upsertPolicyAcl(
+      name,
+      {
+        policy: `path "${projectSlug}/*" { capabilities = ["create", "read", "update", "delete", "list"] }`,
+      },
+    )
+  }
+
+  async createAppAdminPolicy(name: string, projectSlug: string) {
+    await this.vaultClientService.upsertPolicyAcl(
+      name,
+      {
+        policy:
+      `path "${this.config.vaultKvName}/data/${this.config.projectRootPath}/${projectSlug}/REGISTRY/ro-robot" { capabilities = ["read"] }`,
+      },
+    )
   }
 }
