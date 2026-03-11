fab29adb4f17142e6c9fc392c1853fb2ac039712
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-11 12:26:26 +0100

    chore(vault): migrate Vault plugin to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/modules/vault/vault.utils.ts
@@ -0,0 +1,13 @@
+import type { ProjectWithDetails } from './vault-datastore.service'
+
+export function generateTechnicalReadOnlyPolicyName(project: ProjectWithDetails) {
+  return `tech--${project.slug}--ro`
+}
+
+export function generateAppAdminPolicyName(project: ProjectWithDetails) {
+  return `app--${project.slug}--admin`
+}
+
+export function generateZoneName(name: string) {
+  return `zone-${name}`
+}
