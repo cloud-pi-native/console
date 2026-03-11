3ef028bcc556df1446c39e4c602a7a629fd2e0a1
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-11 12:19:52 +0100

    head Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/modules/vault/vault.module.ts
+++ apps/server-nestjs/src/modules/vault/vault.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common'
-import { ConfigurationModule } from '../../cpin-module/infrastructure/configuration/configuration.module'
+import { ConfigurationModule } from '@/cpin-module/infrastructure/configuration/configuration.module'
+import { InfrastructureModule } from '@/cpin-module/infrastructure/infrastructure.module'
 import { VaultClientService } from './vault-client.service'
+import { VaultControllerService } from './vault-controller.service'
+import { VaultDatastoreService } from './vault-datastore.service'
 import { VaultService } from './vault.service'
 
 @Module({
-  imports: [ConfigurationModule],
-  providers: [VaultService, VaultClientService],
+  imports: [ConfigurationModule, InfrastructureModule],
+  providers: [VaultService, VaultClientService, VaultControllerService, VaultDatastoreService],
   exports: [VaultService],
 })
 export class VaultModule {}
