fb3cf8ed1877d30472f306d105237e0abe904b8a
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-11 09:23:57 +0100

    chore(vault): migrate Vault plugin to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/modules/vault/vault-datastore.service.ts
@@ -0,0 +1,43 @@
+import { Inject, Injectable, Logger } from '@nestjs/common'
+import type { Prisma } from '@prisma/client'
+import { PrismaService } from '@/cpin-module/infrastructure/database/prisma.service'
+
+export const projectSelect = {
+  id: true,
+  name: true,
+  slug: true,
+  description: true,
+  environments: {
+    select: {
+      id: true,
+      name: true,
+      clusterId: true,
+      cpu: true,
+      gpu: true,
+      memory: true,
+      autosync: true,
+    },
+  },
+} satisfies Prisma.ProjectSelect
+
+export type ProjectWithDetails = Prisma.ProjectGetPayload<{
+  select: typeof projectSelect
+}>
+
+@Injectable()
+export class GitlabDatastoreService {
+  constructor(@Inject(PrismaService) private readonly prisma: PrismaService) {}
+
+  async getAllProjects(): Promise<ProjectWithDetails[]> {
+    return this.prisma.project.findMany({
+      select: projectSelect,
+    })
+  }
+
+  async getProject(id: string): Promise<ProjectWithDetails | null> {
+    return this.prisma.project.findUnique({
+      where: { id },
+      select: projectSelect,
+    })
+  }
+}
