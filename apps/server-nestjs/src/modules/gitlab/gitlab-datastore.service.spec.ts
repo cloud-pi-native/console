b6dabf200ecdb95e69a73f6e836070e0af81881c
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-12 18:29:51 +0100

    refactor(argocd): migrate ArgoCD to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/modules/gitlab/gitlab-datastore.service.spec.ts
@@ -0,0 +1,36 @@
+import { Test } from '@nestjs/testing'
+import type { TestingModule } from '@nestjs/testing'
+import { GitlabDatastoreService } from './gitlab-datastore.service'
+import { PrismaService } from '@/cpin-module/infrastructure/database/prisma.service'
+import { mockDeep } from 'vitest-mock-extended'
+import { describe, beforeEach, it, expect } from 'vitest'
+
+const prismaMock = mockDeep<PrismaService>()
+
+function createGitlabDatastoreServiceTestingModule() {
+  return Test.createTestingModule({
+    providers: [
+      GitlabDatastoreService,
+      { provide: PrismaService, useValue: prismaMock },
+    ],
+  })
+}
+
+describe('gitlabDatastoreService', () => {
+  let service: GitlabDatastoreService
+
+  beforeEach(async () => {
+    const module: TestingModule = await createGitlabDatastoreServiceTestingModule().compile()
+    service = module.get(GitlabDatastoreService)
+  })
+
+  it('should be defined', () => {
+    expect(service).toBeDefined()
+  })
+
+  it('should get user', async () => {
+    const user = { id: 'user-id' }
+    prismaMock.user.findUnique.mockResolvedValue(user as any)
+    await expect(service.getUser('user-id')).resolves.toEqual(user)
+  })
+})
