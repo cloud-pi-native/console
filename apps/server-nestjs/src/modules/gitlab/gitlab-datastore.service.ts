39088bbec46fdfdba0ff93c83902c00e18bb74da
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-12 15:53:57 +0100

    refactor(argocd): migrate ArgoCD to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/modules/gitlab/gitlab-datastore.service.ts
@@ -0,0 +1,103 @@
+import { Inject, Injectable } from '@nestjs/common'
+import type { Prisma } from '@prisma/client'
+import { PrismaService } from '@/cpin-module/infrastructure/database/prisma.service'
+
+export const projectSelect = {
+  id: true,
+  name: true,
+  slug: true,
+  description: true,
+  owner: {
+    select: {
+      id: true,
+      email: true,
+      firstName: true,
+      lastName: true,
+      adminRoleIds: true,
+    },
+  },
+  plugins: {
+    select: {
+      key: true,
+      value: true,
+    },
+  },
+  roles: {
+    select: {
+      id: true,
+      oidcGroup: true,
+    },
+  },
+  members: {
+    select: {
+      user: {
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          adminRoleIds: true,
+        },
+      },
+      roleIds: true,
+    },
+  },
+  repositories: {
+    select: {
+      id: true,
+      internalRepoName: true,
+      isInfra: true,
+      isPrivate: true,
+      externalRepoUrl: true,
+      externalUserName: true,
+    },
+  },
+  clusters: {
+    select: {
+      id: true,
+      label: true,
+      zone: {
+        select: {
+          slug: true,
+        },
+      },
+    },
+  },
+} satisfies Prisma.ProjectSelect
+
+export type ProjectWithDetails = Prisma.ProjectGetPayload<{
+  select: typeof projectSelect
+}>
+
+@Injectable()
+export class GitlabDatastoreService {
+  constructor(@Inject(PrismaService) private readonly prisma: PrismaService) {}
+
+  async getAllProjects(): Promise<ProjectWithDetails[]> {
+    return this.prisma.project.findMany({
+      select: projectSelect,
+      where: {
+        plugins: {
+          some: {
+            pluginName: 'gitlab',
+          },
+        },
+      },
+    })
+  }
+
+  async getProject(id: string): Promise<ProjectWithDetails | null> {
+    return this.prisma.project.findUnique({
+      where: { id },
+      select: projectSelect,
+    })
+  }
+
+  async getUser(id: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+  }
+}
