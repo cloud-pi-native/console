4963635ada9f3a2cd85d3bceb3d89c12df8f1727
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-12 15:32:20 +0100

    refactor(argocd): migrate ArgoCD to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/modules/vault/vault.utils.ts
@@ -0,0 +1,17 @@
+import type { ProjectWithDetails } from './vault-datastore.service'
+
+export function generateTechnicalReadOnlyPolicyName(project: ProjectWithDetails) {
+  return `tech--${project.slug}--ro`
+}
+
+export function generateAppAdminPolicyName(project: ProjectWithDetails) {
+  return `app--${project.slug}--admin`
+}
+
+export function generateZoneName(name: string) {
+  return `zone-${name}`
+}
+
+export function generateZoneTechnicalReadOnlyPolicyName(zoneName: string) {
+  return `tech--${generateZoneName(zoneName)}--ro`
+}
