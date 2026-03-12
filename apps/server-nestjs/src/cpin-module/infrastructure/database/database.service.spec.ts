a85a5460ab3c790881d9f77bb24894cf2abd727a
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-12 11:57:03 +0100

    fix(server-nestjs): explicit test utils import Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/cpin-module/infrastructure/database/database.service.spec.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/database/database.service.spec.ts
@@ -1,5 +1,6 @@
 import type { TestingModule } from '@nestjs/testing'
 import { Test } from '@nestjs/testing'
+import { describe, beforeEach, it, expect } from 'vitest'
 
 import { DatabaseService } from './database.service'
 import { ConfigurationModule } from '../configuration/configuration.module'
