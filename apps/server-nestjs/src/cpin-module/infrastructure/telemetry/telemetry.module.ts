779a518d1f67eff738b2699ae255664126163f16
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-13 18:04:37 +0100

    feat(telemetry): add decorator based tracing utils
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/cpin-module/infrastructure/telemetry/telemetry.module.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/telemetry/telemetry.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common'
+import { trace } from '@opentelemetry/api'
 import { TelemetryService } from './telemetry.service'
+import { TELEMETRY_TRACER, TELEMETRY_TRACER_NAME } from './telemetry.types'
 
 @Module({
-  providers: [TelemetryService],
+  providers: [
+    TelemetryService,
+    {
+      provide: TELEMETRY_TRACER,
+      useFactory: () => trace.getTracer(TELEMETRY_TRACER_NAME),
+    },
+  ],
+  exports: [TelemetryService, TELEMETRY_TRACER],
 })
 export class TelemetryModule {}
