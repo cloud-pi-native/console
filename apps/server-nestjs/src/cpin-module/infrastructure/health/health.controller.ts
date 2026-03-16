d278e611e75a12ad2232d7de448c1815c09ec406
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-16 16:47:03 +0100

    refactor(argocd): migrate ArgoCD to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/cpin-module/infrastructure/health/health.controller.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/health/health.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get, Inject } from '@nestjs/common'
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus'
 import { DatabaseHealthService } from '@/cpin-module/infrastructure/database/database-health.service'
+import { ArgoCDHealthService } from '../../../modules/argocd/argocd-health.service'
+import { GitlabHealthService } from '../../../modules/gitlab/gitlab-health.service'
 import { KeycloakHealthService } from '../../../modules/keycloak/keycloak-health.service'
+import { VaultHealthService } from '../../../modules/vault/vault-health.service'
 
 @Controller('health')
 export class HealthController {
@@ -9,6 +12,9 @@ export class HealthController {
     @Inject(HealthCheckService) private readonly health: HealthCheckService,
     @Inject(DatabaseHealthService) private readonly database: DatabaseHealthService,
     @Inject(KeycloakHealthService) private readonly keycloak: KeycloakHealthService,
+    @Inject(GitlabHealthService) private readonly gitlab: GitlabHealthService,
+    @Inject(VaultHealthService) private readonly vault: VaultHealthService,
+    @Inject(ArgoCDHealthService) private readonly argocd: ArgoCDHealthService,
   ) {}
 
   @Get()
@@ -17,6 +23,9 @@ export class HealthController {
     return this.health.check([
       () => this.database.check('database'),
       () => this.keycloak.check('keycloak'),
+      () => this.gitlab.check('gitlab'),
+      () => this.vault.check('vault'),
+      () => this.argocd.check('argocd'),
     ])
   }
 }
