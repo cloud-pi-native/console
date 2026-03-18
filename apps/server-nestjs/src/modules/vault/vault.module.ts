4fdc5869f0bafdeb99ea84d70381313cd11049bd
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-18 12:19:18 +0100

    refactor(argocd): migrate ArgoCD to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/modules/vault/vault.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common'
+import { ConfigurationModule } from '@/cpin-module/infrastructure/configuration/configuration.module'
+import { InfrastructureModule } from '@/cpin-module/infrastructure/infrastructure.module'
+import { VaultClientService } from './vault-client.service'
+import { VaultControllerService } from './vault-controller.service'
+import { VaultDatastoreService } from './vault-datastore.service'
+import { VaultService } from './vault.service'
+import { VaultHealthService } from './vault-health.service'
+
+@Module({
+  imports: [ConfigurationModule, InfrastructureModule],
+  providers: [VaultService, VaultClientService, VaultControllerService, VaultDatastoreService],
+  exports: [VaultService, VaultHealthService],
+})
+export class VaultModule {}
