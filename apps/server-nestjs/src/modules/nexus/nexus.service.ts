c2497798d6192edec8c409e1d8a19abb3b03c008
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-19 14:54:53 +0100

    refactor(server-nestjs): migrate package managers to NestJS
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/modules/nexus/nexus.service.ts
@@ -0,0 +1,24 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { ConfigurationService } from '../../cpin-module/infrastructure/configuration/configuration.service'
+import { generateMavenHostedRepoName, generateNpmHostedRepoName } from './nexus.utils'
+
+@Injectable()
+export class NexusService {
+  constructor(
+    @Inject(ConfigurationService) private readonly config: ConfigurationService,
+  ) {}
+
+  getProjectSecrets(args: { projectSlug: string, enableMaven: boolean, enableNpm: boolean }) {
+    const projectSlug = args.projectSlug
+    const nexusUrl = this.config.nexusSecretExposedUrl!
+    const secrets: Record<string, string> = {}
+    if (args.enableMaven) {
+      secrets.MAVEN_REPO_RELEASE = `${nexusUrl}/${generateMavenHostedRepoName(projectSlug, 'release')}`
+      secrets.MAVEN_REPO_SNAPSHOT = `${nexusUrl}/${generateMavenHostedRepoName(projectSlug, 'snapshot')}`
+    }
+    if (args.enableNpm) {
+      secrets.NPM_REPO = `${nexusUrl}/${generateNpmHostedRepoName(projectSlug)}`
+    }
+    return secrets
+  }
+}
