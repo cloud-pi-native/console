25643beeb1415f3aec00b25f44333d1e3b09e381
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-19 15:51:18 +0100

    refactor(keycloak): migrate Keycloak plugin to NestJS
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/cpin-module/infrastructure/infrastructure.module.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/infrastructure.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 
 import { ConfigurationModule } from './configuration/configuration.module'
 import { DatabaseService } from './database/database.service'
+import { PrismaService } from './database/prisma.service'
 import { HealthModule } from './health/health.module'
 import { HttpClientService } from './http-client/http-client.service'
 import { LoggerModule } from './logger/logger.module'
@@ -9,8 +10,8 @@ import { ServerService } from './server/server.service'
 import { TelemetryModule } from './telemetry/telemetry.module'
 
 @Module({
-  providers: [DatabaseService, HttpClientService, ServerService],
+  providers: [DatabaseService, PrismaService, HttpClientService, ServerService],
   imports: [LoggerModule, ConfigurationModule, TelemetryModule, HealthModule],
-  exports: [DatabaseService, HttpClientService, ServerService],
+  exports: [DatabaseService, PrismaService, HttpClientService, ServerService],
 })
 export class InfrastructureModule {}
