03b972df3381ca35f43eeab0562a1b9b55db96f2
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-20 12:32:34 +0100

    chore: migrate cypress e2e tests to playwright Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ playwright/e2e-tests/admin-logs.spec.ts
@@ -0,0 +1,35 @@
+import { expect, test } from '@playwright/test'
+
+import { clientURL, signInCloudPiNative, tcolinUser } from '../config/console'
+
+test.describe('Admin - Logs', () => {
+  test('should display logs and paginate', { tag: '@e2e' }, async ({ page }) => {
+    await page.goto(clientURL)
+    await signInCloudPiNative({ page, credentials: tcolinUser })
+
+    await page.getByTestId('menuAdministrationBtn').click()
+    await page.getByTestId('menuAdministrationLogs').click()
+
+    await expect(page.locator('h1')).toContainText(
+      'Journaux des services associés à la chaîne DSO',
+    )
+    await expect(page.getByTestId('logCountInfo')).toBeVisible()
+    await expect(page.getByTestId('positionInfo')).toBeVisible()
+
+    await expect(page.getByTestId('seePreviousPageBtn')).toBeDisabled()
+    await expect(page.getByTestId('seeFirstPageBtn')).toBeDisabled()
+    await expect(page.getByTestId('seeNextPageBtn')).toBeEnabled()
+    await expect(page.getByTestId('seeLastPageBtn')).toBeEnabled()
+
+    await expect(page.locator('[data-testid$="-json"]')).toHaveCount(10)
+
+    await page.getByTestId('seeNextPageBtn').first().click()
+    await expect(page.getByTestId('seePreviousPageBtn')).toBeEnabled()
+    await expect(page.getByTestId('seeFirstPageBtn')).toBeEnabled()
+
+    await page.getByTestId('showLogsBtn').click()
+    await expect(page.locator('[data-testid$="-json"]')).toHaveCount(0)
+    await page.getByTestId('showLogsBtn').click()
+    await expect(page.locator('[data-testid$="-json"]')).not.toHaveCount(0)
+  })
+})
