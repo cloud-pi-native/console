5debab59f081b15d831cb3c4340b0186f2e87a1e
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-23 18:18:51 +0100

    chore(version): migrate to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/main.module.ts
+++ apps/server-nestjs/src/main.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { CpinModule } from './cpin-module/cpin.module'
 import { HealthzModule } from './modules/healthz/healthz.module'
+import { VersionModule } from './modules/version/version.module'
 
 @Module({
-  imports: [CpinModule, HealthzModule],
+  imports: [CpinModule, HealthzModule, VersionModule],
   controllers: [],
   providers: [],
 })
