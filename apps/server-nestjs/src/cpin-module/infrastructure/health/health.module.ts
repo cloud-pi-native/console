5de7d102e572ed213aeaad26406dcf2d60a9d4fd
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-23 15:27:21 +0100

    refactor(server-nestjs): migrate package managers to NestJS
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/cpin-module/infrastructure/health/health.module.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/health/health.module.ts
@@ -3,6 +3,8 @@ import { TerminusModule } from '@nestjs/terminus'
 import { ArgoCDModule } from '../../../modules/argocd/argocd.module'
 import { GitlabModule } from '../../../modules/gitlab/gitlab.module'
 import { KeycloakModule } from '../../../modules/keycloak/keycloak.module'
+import { NexusModule } from '../../../modules/nexus/nexus.module'
+import { RegistryModule } from '../../../modules/registry/registry.module'
 import { VaultModule } from '../../../modules/vault/vault.module'
 import { DatabaseModule } from '../database/database.module'
 import { HealthController } from './health.controller'
@@ -14,6 +16,8 @@ import { HealthController } from './health.controller'
     KeycloakModule,
     GitlabModule,
     VaultModule,
+    NexusModule,
+    RegistryModule,
     ArgoCDModule,
   ],
   controllers: [HealthController],
