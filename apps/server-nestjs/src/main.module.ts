5cdb0b385243ccd1537801078443bd01ee99957c
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-23 16:56:09 +0100

    chore(system-settings): migrate to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/main.module.ts
+++ apps/server-nestjs/src/main.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { CpinModule } from './cpin-module/cpin.module'
 import { HealthzModule } from './modules/healthz/healthz.module'
+import { SystemSettingsModule } from './modules/system-settings/system-settings.module'
 
 @Module({
-  imports: [CpinModule, HealthzModule],
+  imports: [CpinModule, HealthzModule, SystemSettingsModule],
   controllers: [],
   providers: [],
 })
---
+++ apps/server-nestjs/src/modules/system-settings/system-settings.controller.ts
@@ -0,0 +1,19 @@
+import type { ListSystemSettingsQueryDto } from './dto/list-system-settings-query.dto'
+import type { SystemSettingDto } from './dto/system-setting.dto'
+import { Body, Controller, Get, Inject, Put, Query } from '@nestjs/common'
+import { SystemSettingsService } from './system-settings.service'
+
+@Controller('api/v1/system/settings')
+export class SystemSettingsController {
+  constructor(@Inject(SystemSettingsService) private readonly service: SystemSettingsService) {}
+
+  @Get()
+  async list(@Query() query: ListSystemSettingsQueryDto) {
+    return this.service.list(query.key)
+  }
+
+  @Put()
+  async upsert(@Body() body: SystemSettingDto) {
+    return this.service.upsert(body)
+  }
+}
