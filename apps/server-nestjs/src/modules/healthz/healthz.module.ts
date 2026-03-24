9dc89c531654ff624f1a10a9bc1b9ac8bc13d532
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-24 17:37:53 +0100

    refactor(argocd): migrate ArgoCD to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/modules/healthz/healthz.module.ts
+++ apps/server-nestjs/src/modules/healthz/healthz.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common'
 import { TerminusModule } from '@nestjs/terminus'
+import { DatabaseModule } from '../../cpin-module/infrastructure/database/database.module'
+import { ArgoCDModule } from '../argocd/argocd.module'
+import { GitlabModule } from '../gitlab/gitlab.module'
 import { KeycloakModule } from '../keycloak/keycloak.module'
 import { DatabaseModule } from '../../cpin-module/infrastructure/database/database.module'
 import { HealthzController } from './healthz.controller'
@@ -9,6 +12,9 @@ import { HealthzController } from './healthz.controller'
     TerminusModule,
     DatabaseModule,
     KeycloakModule,
+    GitlabModule,
+    VaultModule,
+    ArgoCDModule,
   ],
   controllers: [HealthzController],
 })
