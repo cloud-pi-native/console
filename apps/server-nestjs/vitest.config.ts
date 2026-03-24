6937c6ccbefe324274819a2d2b28a574eb82aab4
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-24 17:34:30 +0100

    chore: add vitest
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/vitest.config.ts
@@ -0,0 +1,14 @@
+import path from 'node:path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.spec.ts', 'test/**/*.e2e-spec.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+})
