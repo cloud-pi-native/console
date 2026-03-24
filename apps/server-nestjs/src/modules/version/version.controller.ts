aa481a8a0c2fbd76dfa26a929e988612a9740a0d
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-24 16:04:42 +0100

    chore(version): migrate to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/modules/version/version.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Inject } from '@nestjs/common'
+import { ConfigurationService } from '../../cpin-module/infrastructure/configuration/configuration.service'
+
+@Controller('api/v1/version')
+export class VersionController {
+  constructor(
+    @Inject(ConfigurationService)
+    private readonly config: ConfigurationService,
+  ) {}
+
+  @Get()
+  getVersion() {
+    return { version: this.config.appVersion }
+  }
+}
