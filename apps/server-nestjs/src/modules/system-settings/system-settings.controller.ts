2bb2d098c6db61418636b564e0b346675fa62f78
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-25 11:32:58 +0100

    chore(system-settings): migrate to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/modules/system-settings/system-settings.controller.ts
@@ -0,0 +1,25 @@
+import type { ListSystemSettingsQueryDto } from './dto/list-system-settings-query.dto'
+import type { SystemSettingDto } from './dto/system-setting.dto'
+import { Body, Controller, Get, Inject, Put, Query } from '@nestjs/common'
+import { SystemSettingsService } from './system-settings.service'
+
+@Controller('api/v1/system/settings')
+export class SystemSettingsController {
+  constructor(@Inject(SystemSettingsService) private readonly service: SystemSettingsService) {}
+
+  @Get()
+  @UseGuards(new AbilityGuard('read', 'SystemSetting'))
+  async list(
+    @Query() query: ListSystemSettingsQueryDto,
+  ) {
+    return this.service.list(query.key)
+  }
+
+  @Put(':key')
+  async upsert(
+    @Param('key') key: string,
+    @Body() body: SystemSettingDto,
+  ) {
+    return this.service.upsert(key, body)
+  }
+}
