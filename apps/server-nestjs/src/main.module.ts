76fe58aee4b4de35a379ced300f11f493eebaa51
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-26 17:00:49 +0100

    refactor(keycloak): migrate Keycloak plugin to NestJS
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/main.module.ts
+++ apps/server-nestjs/src/main.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common'
+import { EventEmitterModule } from '@nestjs/event-emitter'
+import { ScheduleModule } from '@nestjs/schedule'
 import { CpinModule } from './cpin-module/cpin.module'
 import { HealthzModule } from './modules/healthz/healthz.module'
+import { KeycloakModule } from './modules/keycloak/keycloak.module'
 import { VersionModule } from './modules/version/version.module'
 
 @Module({
-  imports: [CpinModule, HealthzModule, VersionModule],
+  imports: [
+    CpinModule,
+    KeycloakModule,
+    HealthzModule,
+    VersionModule,
+    EventEmitterModule.forRoot(),
+    ScheduleModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
