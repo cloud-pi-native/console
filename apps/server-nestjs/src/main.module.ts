37d11cdd69638e7cd54379c605e83971770941e3
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-26 11:06:51 +0100

    chore(system-settings): migrate to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/main.module.ts
+++ apps/server-nestjs/src/main.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { CpinModule } from './cpin-module/cpin.module'
 import { HealthzModule } from './modules/healthz/healthz.module'
+import { SystemSettingsModule } from './modules/system-settings/system-settings.module'
 import { VersionModule } from './modules/version/version.module'
 
 @Module({
-  imports: [CpinModule, HealthzModule, VersionModule],
+  imports: [CpinModule, HealthzModule, SystemSettingsModule, VersionModule],
   controllers: [],
   providers: [],
 })
