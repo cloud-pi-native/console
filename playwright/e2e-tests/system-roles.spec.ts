56f1640042f5b0681dbd0b4d021208a2fc285e32
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-03-31 09:58:55 +0200

    feat: segregate system role with prefix type as protected resources
    If the usable bubbles up, such as using cpin as an identity provider at the application level, we may re-add the OIDC group and type configuration in the client explicitly.
    But for the moment and the user experience we decided to hide those two fields.
    
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ playwright/e2e-tests/system-roles.spec.ts
@@ -0,0 +1,42 @@
+import { expect, test } from '@playwright/test'
+import {
+  clientURL,
+  signInCloudPiNative,
+  testUser,
+} from '../config/console'
+import {
+  addProject,
+} from './utils'
+
+test.describe('System Roles at Project Creation', () => {
+  test('Should have built-in system roles after project creation', { tag: '@e2e' }, async ({ page }) => {
+    // Arrange
+    await page.goto(clientURL)
+    await signInCloudPiNative({ page, credentials: testUser })
+    await addProject({ page })
+
+    // Act
+    await page.getByTestId('test-tab-roles').click()
+
+    // Assert
+    const systemRoles = [
+      { name: 'Administrateur' },
+      { name: 'DevOps' },
+      { name: 'Développeur' },
+      { name: 'Lecture seule' },
+    ]
+
+    for (const role of systemRoles) {
+      // Click on the role to select it
+      const roleElement = await page.getByText(role.name)
+      await expect(roleElement).toBeVisible()
+      await roleElement.click()
+
+      // Check name input is disabled
+      await expect(page.getByTestId('roleNameInput')).toHaveValue(role.name)
+
+      // Check delete button is not visible
+      await expect(page.getByTestId('deleteBtn')).toBeHidden()
+    }
+  })
+})
