9f6e14e34489476a4fa41ca20e5941f6b7bfe591
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-04-07 15:58:05 +0200

    feat: add otel Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/cpin-module/infrastructure/telemetry/telemetry.service.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/telemetry/telemetry.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@nestjs/common'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto'
-import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core'
-import { PinoInstrumentation } from '@opentelemetry/instrumentation-pino'
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics'
 import { NodeSDK } from '@opentelemetry/sdk-node'
 import { SERVICE_NAME } from './telemetry.constants'
@@ -19,8 +17,6 @@ export class TelemetryService extends NodeSDK implements OnModuleInit, OnApplica
       }),
       instrumentations: [
         getNodeAutoInstrumentations(),
-        new NestInstrumentation(),
-        new PinoInstrumentation(),
       ],
       serviceName: SERVICE_NAME,
     })
