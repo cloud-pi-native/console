0dc01fd57807c71e1ac4b66f85c65c7791615ffe
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-04-14 15:49:22 +0200

    chore: remove dotenv dep Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>

--- apps/server-nestjs/prisma.config.ts
+++ apps/server-nestjs/prisma.config.ts
@@ -1,18 +1,11 @@
 import path from 'node:path'
-import * as dotenv from 'dotenv'
 import { defineConfig } from 'prisma/config'
 
-if (process.env.DOCKER !== 'true') {
-  dotenv.config({ path: '.env' })
-}
+if (process.env.INTEGRATION === 'true' && fs.existsSync('.env.integ'))
+  process.loadEnvFile('.env.integ')
 
-if (process.env.INTEGRATION === 'true') {
-  const envInteg = dotenv.config({ path: '.env.integ' })
-  process.env = {
-    ...process.env,
-    ...(envInteg?.parsed ?? {}),
-  }
-}
+if (process.env.DOCKER !== 'true' && fs.existsSync('.env'))
+  process.loadEnvFile('.env')
 
 export default defineConfig({
   schema: path.join('src', 'prisma', 'schema'),
