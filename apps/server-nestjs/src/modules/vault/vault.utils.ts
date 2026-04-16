cae72f684fbed1f13b424af2c7f6870fcac7294e
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-04-16 15:23:47 +0200

    refactor(argocd): migrate ArgoCD to NestJS Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/modules/vault/vault.utils.ts
@@ -0,0 +1,17 @@
+export function generateProjectPath(projectRootDir: string | undefined, projectSlug: string) {
+  return projectRootDir
+    ? `${projectRootDir}/${projectSlug}`
+    : projectSlug
+}
+
+export function generateGitlabMirrorCredPath(projectRootDir: string | undefined, projectSlug: string, repoName: string) {
+  return projectRootDir
+    ? `${generateProjectPath(projectRootDir, projectSlug)}/${repoName}-mirror`
+    : `${projectSlug}/${repoName}-mirror`
+}
+
+export function generateTechReadOnlyCredPath(projectRootDir: string | undefined, projectSlug: string) {
+  return projectRootDir
+    ? `${generateProjectPath(projectRootDir, projectSlug)}/tech/GITLAB_MIRROR`
+    : `${projectSlug}/tech/GITLAB_MIRROR`
+}
