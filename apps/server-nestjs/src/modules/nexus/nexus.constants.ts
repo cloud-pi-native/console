e81d48ca5a4a5e88ce8077afa076738439eff728
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-04-20 16:34:36 +0200

    refactor(server-nestjs): migrate package managers to NestJS
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
    Change-Id: I37010cbbfad6e0ac2dbd2adb8e560be36a6a6964

---
+++ apps/server-nestjs/src/modules/nexus/nexus.constants.ts
@@ -0,0 +1,11 @@
+export const NEXUS_PLUGIN_NAME = 'nexus'
+
+export const NEXUS_CONFIG_KEY_ACTIVATE_MAVEN_REPO = 'activateMavenRepo'
+export const NEXUS_CONFIG_KEY_ACTIVATE_NPM_REPO = 'activateNpmRepo'
+export const NEXUS_CONFIG_KEY_MAVEN_SNAPSHOT_WRITE_POLICY = 'mavenSnapshotWritePolicy'
+export const NEXUS_CONFIG_KEY_MAVEN_RELEASE_WRITE_POLICY = 'mavenReleaseWritePolicy'
+export const NEXUS_CONFIG_KEY_NPM_WRITE_POLICY = 'npmWritePolicy'
+
+export const DEFAULT_MAVEN_SNAPSHOT_WRITE_POLICY = 'allow'
+export const DEFAULT_MAVEN_RELEASE_WRITE_POLICY = 'allow_once'
+export const DEFAULT_NPM_WRITE_POLICY = 'allow'
