c2ac0c8dd0b04a66ff73ff177ce5877d91f0fbd5
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-04-20 09:57:34 +0200

    refactor(server-nestjs): migrate package managers to NestJS
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
    Change-Id: I37010cbbfad6e0ac2dbd2adb8e560be36a6a6964

---
+++ apps/server-nestjs/src/modules/registry/registry.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common'
+import { HealthIndicatorService } from '@nestjs/terminus'
+import { ConfigurationModule } from '../../cpin-module/infrastructure/configuration/configuration.module'
+import { InfrastructureModule } from '../../cpin-module/infrastructure/infrastructure.module'
+import { VaultModule } from '../vault/vault.module'
+import { RegistryClientService } from './registry-client.service'
+import { RegistryDatastoreService } from './registry-datastore.service'
+import { RegistryHealthService } from './registry-health.service'
+import { RegistryHttpClientService } from './registry-http-client.service'
+import { RegistryService } from './registry.service'
+
+@Module({
+  imports: [ConfigurationModule, InfrastructureModule, VaultModule],
+  providers: [HealthIndicatorService, RegistryHealthService, RegistryService, RegistryDatastoreService, RegistryHttpClientService, RegistryClientService],
+  exports: [RegistryHealthService, RegistryService],
+})
+export class RegistryModule {}
