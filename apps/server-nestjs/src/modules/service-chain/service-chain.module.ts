28ce0d7b82a2e812dd87bc7ce3b3d394300e1b86
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2026-04-21 09:59:54 +0200

    chore(server-nestjs): disable auth guard on service-chain (waiting on keycloak module implem)

--- apps/server-nestjs/src/modules/service-chain/service-chain.module.ts
+++ apps/server-nestjs/src/modules/service-chain/service-chain.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
+import { AuthModule } from '../../cpin-module/infrastructure/auth/auth.module'
 import { ConfigurationModule } from '../../cpin-module/infrastructure/configuration/configuration.module'
 import { OpenCdsClientService } from './open-cds-client.service'
 import { ServiceChainController } from './service-chain.controller'
 import { ServiceChainService } from './service-chain.service'
 
 @Module({
-  imports: [ConfigurationModule],
+  imports: [ConfigurationModule, AuthModule],
   controllers: [ServiceChainController],
   providers: [OpenCdsClientService, ServiceChainService],
 })
