742c481b810976a229152e77036c5b5ef9326441
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-04-21 11:58:10 +0200

    feat: add Vault support for fined grained permissions Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

---
+++ apps/server-nestjs/src/modules/vault/vault.constant.ts
@@ -0,0 +1,28 @@
+export const VAULT_PLUGIN_NAME = 'vault'
+
+export const PLATFORM_ADMIN_POLICY_NAME = 'platform--admin'
+export const PLATFORM_READONLY_POLICY_NAME = 'platform--readonly'
+export const PLATFORM_SECURITY_POLICY_NAME = 'platform--security'
+
+export const CONSOLE_ADMIN_GROUP_NAME = 'console-admin'
+export const CONSOLE_READONLY_GROUP_NAME = 'console-readonly'
+export const CONSOLE_SECURITY_GROUP_NAME = 'console-security'
+
+export const DEFAULT_ADMIN_GROUP_PATH = '/console/admin'
+export const DEFAULT_AUDITOR_GROUP_PATH = '/console/readonly'
+export const DEFAULT_SECURITY_GROUP_PATH = '/console/security'
+
+export const DEFAULT_PROJECT_MAINTAINER_GROUP_PATH_SUFFIX = '/console/admin'
+export const DEFAULT_PROJECT_DEVELOPER_GROUP_PATH_SUFFIX = '/console/developer'
+export const DEFAULT_PROJECT_DEVOPS_GROUP_PATH_SUFFIX = '/console/devops'
+export const DEFAULT_PROJECT_REPORTER_GROUP_PATH_SUFFIX = '/console/readonly'
+export const DEFAULT_PROJECT_SECURITY_GROUP_PATH_SUFFIX = '/console/security'
+
+export const ADMIN_GROUP_PATH_PLUGIN_KEY = 'adminGroupPath'
+export const AUDITOR_GROUP_PATH_PLUGIN_KEY = 'auditorGroupPath'
+export const SECURITY_GROUP_PATH_PLUGIN_KEY = 'securityGroupPath'
+export const PROJECT_REPORTER_GROUP_PATH_SUFFIX_PLUGIN_KEY = 'projectReporterGroupPathSuffix'
+export const PROJECT_DEVELOPER_GROUP_PATH_SUFFIX_PLUGIN_KEY = 'projectDeveloperGroupPathSuffix'
+export const PROJECT_DEVOPS_GROUP_PATH_SUFFIX_PLUGIN_KEY = 'projectDevopsGroupPathSuffix'
+export const PROJECT_MAINTAINER_GROUP_PATH_SUFFIX_PLUGIN_KEY = 'projectMaintainerGroupPathSuffix'
+export const PROJECT_SECURITY_GROUP_PATH_SUFFIX_PLUGIN_KEY = 'projectSecurityGroupPathSuffix'
