4f3ffb3a970a7f40394758162782d9bb78069123
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-04-21 12:29:43 +0200

    refactor(server-nestjs): migrate package managers to NestJS
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
    Change-Id: I37010cbbfad6e0ac2dbd2adb8e560be36a6a6964

---
+++ apps/server-nestjs/src/modules/registry/registry.constants.ts
@@ -0,0 +1,4 @@
+export const REGISTRY_PLUGIN_NAME = 'harbor'
+
+export const REGISTRY_CONFIG_KEY_QUOTA_HARD_LIMIT = 'quotaHardLimit'
+export const REGISTRY_CONFIG_KEY_PUBLISH_PROJECT_ROBOT = 'publishProjectRobot'
