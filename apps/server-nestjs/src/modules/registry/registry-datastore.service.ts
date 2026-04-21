c672393ab5e78d4d86fad3bafa8d029d2dd0d22f
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-04-21 12:08:00 +0200

    refactor(server-nestjs): migrate package managers to NestJS
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
    Change-Id: I37010cbbfad6e0ac2dbd2adb8e560be36a6a6964

---
+++ apps/server-nestjs/src/modules/registry/registry-datastore.service.ts
@@ -0,0 +1,46 @@
+import type { Prisma } from '@prisma/client'
+import { Inject, Injectable } from '@nestjs/common'
+import { PrismaService } from '../../cpin-module/infrastructure/database/prisma.service'
+import { REGISTRY_PLUGIN_NAME } from './registry.constants'
+
+export const projectSelect = {
+  slug: true,
+  plugins: {
+    where: {
+      pluginName: REGISTRY_PLUGIN_NAME,
+    },
+    select: {
+      key: true,
+      value: true,
+    },
+  },
+} satisfies Prisma.ProjectSelect
+
+export type ProjectWithDetails = Prisma.ProjectGetPayload<{
+  select: typeof projectSelect
+}>
+
+@Injectable()
+export class RegistryDatastoreService {
+  constructor(@Inject(PrismaService) private readonly prisma: PrismaService) {}
+
+  async getAllProjects(): Promise<ProjectWithDetails[]> {
+    return await this.prisma.project.findMany({
+      select: projectSelect,
+      where: {
+        plugins: {
+          some: {
+            pluginName: REGISTRY_PLUGIN_NAME,
+          },
+        },
+      },
+    })
+  }
+
+  async getProject(id: string): Promise<ProjectWithDetails | null> {
+    return await this.prisma.project.findUnique({
+      where: { id },
+      select: projectSelect,
+    })
+  }
+}
