ec6c582a95868edafd4f9cb45e115bf9da804036
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2026-04-27 08:42:23 +0200

    chore(server-nestjs): remove useless CpinModule core modules

--- apps/server-nestjs/src/cpin-module/core/core.module.ts
+++ apps/server-nestjs/src/cpin-module/core/core.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common'
 
 import { ConfigurationModule } from '../infrastructure/configuration/configuration.module'
 import { InfrastructureModule } from '../infrastructure/infrastructure.module'
-import { AppService } from './app/app.service'
-import { FastifyService } from './fastify/fastify.service'
 
 @Module({
   imports: [ConfigurationModule, InfrastructureModule],
-  providers: [AppService, FastifyService],
+  providers: [],
 })
 export class CoreModule {}
