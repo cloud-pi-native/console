e60bc5610256be503ddf75ec0775c1aece019af6
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-04-27 16:14:33 +0200

    chore: migrate duplicate database implementation in package Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/src/cpin-module/infrastructure/database/prisma.service.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/database/prisma.service.ts
@@ -1,6 +1,6 @@
 import type { OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@cpn-console/database'
 import { Injectable } from '@nestjs/common'
-import { PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
