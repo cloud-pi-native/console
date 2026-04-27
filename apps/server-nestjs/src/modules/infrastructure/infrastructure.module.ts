f50f0e5b59a661d065d448196af14955b9fde9a0
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2026-04-27 14:03:51 +0200

    chore(server-nestjs): remove useless ServerService service

--- apps/server-nestjs/src/modules/infrastructure/infrastructure.module.ts
+++ apps/server-nestjs/src/modules/infrastructure/infrastructure.module.ts
@@ -3,12 +3,11 @@ import { Module } from '@nestjs/common'
 import { ConfigurationModule } from './configuration/configuration.module'
 import { DatabaseModule } from './database/database.module'
 import { LoggerModule } from './logger/logger.module'
-import { ServerService } from './server/server.service'
 import { TelemetryModule } from './telemetry/telemetry.module'
 
 @Module({
-  providers: [ServerService],
+  providers: [],
   imports: [DatabaseModule, LoggerModule, ConfigurationModule, TelemetryModule],
-  exports: [DatabaseModule, ServerService],
+  exports: [DatabaseModule],
 })
 export class InfrastructureModule {}
