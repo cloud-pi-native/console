e065b0fb0d0756d892654915040945ad9ffebb50
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-04-27 15:39:13 +0200

    fix: dotenv doesn't overload existing env var
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server-nestjs/prisma.config.ts
+++ apps/server-nestjs/prisma.config.ts
@@ -1,12 +1,19 @@
 import fs from 'node:fs'
 import path from 'node:path'
+import { parseEnv } from 'node:util'
 import { defineConfig } from 'prisma/config'
 
-if (process.env.INTEGRATION === 'true' && fs.existsSync('.env.integ'))
-  process.loadEnvFile('.env.integ')
+if (process.env.DOCKER !== 'true' && fs.existsSync('.env')) {
+  Object.assign(process.env, Object.fromEntries(
+    Object.entries(parseEnv(fs.readFileSync(path.resolve('.env'), 'utf-8'))).filter(([k, v]) => Boolean(v)),
+  ))
+}
 
-if (process.env.DOCKER !== 'true' && fs.existsSync('.env'))
-  process.loadEnvFile('.env')
+if (process.env.INTEGRATION === 'true' && fs.existsSync('.env.integ')) {
+  Object.assign(process.env, Object.fromEntries(
+    Object.entries(parseEnv(fs.readFileSync(path.resolve('.env.integ'), 'utf-8'))).filter(([_k, v]) => Boolean(v)),
+  ))
+}
 
 export default defineConfig({
   schema: path.join('src', 'prisma', 'schema'),
