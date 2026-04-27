8bf627baf2b17263ce8a8e8544be89c07933c400
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2026-04-27 08:40:39 +0200

    chore(server-nestjs): remove useless HttpClientService

--- apps/server-nestjs/src/cpin-module/infrastructure/infrastructure.module.ts
+++ apps/server-nestjs/src/cpin-module/infrastructure/infrastructure.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common'
 
 import { ConfigurationModule } from './configuration/configuration.module'
 import { DatabaseModule } from './database/database.module'
-import { HttpClientService } from './http-client/http-client.service'
 import { LoggerModule } from './logger/logger.module'
 import { ServerService } from './server/server.service'
 import { TelemetryModule } from './telemetry/telemetry.module'
 
 @Module({
-  providers: [HttpClientService, ServerService],
+  providers: [ServerService],
   imports: [DatabaseModule, LoggerModule, ConfigurationModule, TelemetryModule],
-  exports: [DatabaseModule, HttpClientService, ServerService],
+  exports: [DatabaseModule, ServerService],
 })
 export class InfrastructureModule {}
