20b6fb3101a4b13c6c2cbb9afb50c13eb7789b85
Oussama Miladi <35038682+omiladi@users.noreply.github.com>
2026-05-19 18:01:03 +0200

    fix: sed command quotes in entrypoint.sh

--- apps/nginx-strangler/entrypoint.sh
+++ apps/nginx-strangler/entrypoint.sh
@@ -3,8 +3,8 @@
 ROUTING_CONFIG_FILE=/etc/nginx/conf.d/routing.conf
 
 echo "Replacing env constants in NGINX configuration"
-sed -i "s|\${LEGACY_UPSTREAM}|${LEGACY_UPSTREAM}|g" $ROUTING_CONFIG_FILE
-sed -i "s|\${NESTJS_UPSTREAM}|${NESTJS_UPSTREAM}|g" $ROUTING_CONFIG_FILE
+sed -i 's|'${LEGACY_UPSTREAM}'|'${LEGACY_UPSTREAM}'|g' $ROUTING_CONFIG_FILE
+sed -i 's|'${NESTJS_UPSTREAM}'|'${NESTJS_UPSTREAM}'|g' $ROUTING_CONFIG_FILE
 
 nginx -t
 
