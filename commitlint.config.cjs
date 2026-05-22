5376e784c6a835fd6b17473282aa5f9cb6dd83f5
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2026-05-22 14:40:07 +0200

    chore: forbid commit message body not leading with a blank line
    Commit messages display looks weird because of that.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: [
-    '@commitlint/config-conventional',
-  ],
-}
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [
+      2, // error
+      'always',
+    ],
+  },
+};
