1e39a04374d2bb4b695dde88e20b9ca48c68589c
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-06-09 16:07:32 +0200

    chore: remove unused packages and fix otel configuration
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
    Change-Id: I941eb6668cf8c98cbd8ea8e8510062216a6a6964

--- apps/server-nestjs/src/modules/infrastructure/infrastructure.module.ts
+++ apps/server-nestjs/src/modules/infrastructure/infrastructure.module.ts
@@ -3,11 +3,10 @@ import { Module } from '@nestjs/common'
 import { ConfigurationModule } from './configuration/configuration.module'
 import { DatabaseModule } from './database/database.module'
 import { LoggerModule } from './logger/logger.module'
-import { TelemetryModule } from './telemetry/telemetry.module'
 
 @Module({
   providers: [],
-  imports: [DatabaseModule, LoggerModule, ConfigurationModule, TelemetryModule],
+  imports: [DatabaseModule, LoggerModule, ConfigurationModule],
   exports: [DatabaseModule],
 })
 export class InfrastructureModule {}
