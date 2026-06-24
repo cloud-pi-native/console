6bac041e55258aff658597a3fabbffffc4efcb5d
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-24 16:24:49 +0200

    chore: improve auth logging
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I3ce611361a8a75d5fe82d92767aaac016a6a6964

--- apps/server-nestjs/src/modules/infrastructure/auth/auth.service.ts
+++ apps/server-nestjs/src/modules/infrastructure/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import type { FastifyRequest } from 'fastify'
 import type { AuthRequirements, UserContext } from './dso-token/dso-token.service'
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common'
+import { Inject, Injectable, Logger, UnauthorizedException } from '@nestjs/common'
 import { DsoTokenService } from './dso-token/dso-token.service'
 import { KeycloakJwtService } from './keycloak-jwt/keycloak-jwt.service'
 
@@ -8,6 +8,8 @@ export type { AuthRequirements, AuthToken, UserContext } from './dso-token/dso-t
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name)
+
   constructor(
     @Inject(DsoTokenService) private readonly dsoTokenService: DsoTokenService,
     @Inject(KeycloakJwtService) private readonly keycloakJwtService: KeycloakJwtService,
@@ -19,13 +21,17 @@ export class AuthService {
   ): Promise<UserContext> {
     const dsoTokenResult = await this.dsoTokenService.authenticate(request, requirements)
     if (dsoTokenResult) {
+      this.logger.debug(`Auth authenticated request (requestId=${request.id}, authSource=dso-token, userId=${dsoTokenResult.userId}, adminPermissions=${dsoTokenResult.adminPermissions?.toString()}, userType=${dsoTokenResult.userType})`)
       return dsoTokenResult
     }
 
     const bearerTokenResult = await this.keycloakJwtService.authenticate(request, requirements)
     if (bearerTokenResult) {
+      this.logger.debug(`Auth authenticated request (requestId=${request.id}, authSource=keycloak-jwt, userId=${bearerTokenResult.userId}, adminPermissions=${bearerTokenResult.adminPermissions?.toString()}, userType=${bearerTokenResult.userType})`)
       return bearerTokenResult
     }
+
+    this.logger.warn(`Auth rejected request (requestId=${request.id}, hasDsoTokenHeader=${typeof request.headers['x-dso-token'] === 'string'}, hasAuthorizationHeader=${typeof request.headers.authorization === 'string'})`)
 
     throw new UnauthorizedException('Not authenticated')
   }
