53735522f8c455cf7fd30a626c8ef4be0bb175d5
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-26 15:03:58 +0200

    refactor(user-tokens): migrate from server
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
    Change-Id: Ib1b5cbbfcd8274912ed9ffb20d60cbed6a6a6964

--- apps/server-nestjs/src/main.module.ts
+++ apps/server-nestjs/src/main.module.ts
@@ -7,6 +7,7 @@ import { DeploymentModule } from './modules/deployment/deployment.module'
 import { EnvironmentModule } from './modules/environment/environment.module'
 import { HealthzModule } from './modules/healthz/healthz.module'
 import { InfrastructureModule } from './modules/infrastructure/infrastructure.module'
+import { KeycloakModule } from './modules/keycloak/keycloak.module'
 import { LogModule } from './modules/log/log.module'
 import { PluginModule } from './modules/plugin/plugin.module'
 import { ProjectBulkModule } from './modules/project-bulk/project-bulk.module'
@@ -18,7 +19,9 @@ import { ProjectServicesModule } from './modules/project-services/project-servic
 import { ProjectModule } from './modules/project/project.module'
 import { RepositoryModule } from './modules/repository/repository.module'
 import { SystemConfigModule } from './modules/system-config/system-config.module'
+import { ServiceChainModule } from './modules/service-chain/service-chain.module'
 import { SystemSettingsModule } from './modules/system-settings/system-settings.module'
+import { UserTokensModule } from './modules/user-tokens/user-tokens.module'
 import { VersionModule } from './modules/version/version.module'
 import { getDotenvPaths } from './utils/dotenv.utils'
 
@@ -34,19 +37,22 @@ import { getDotenvPaths } from './utils/dotenv.utils'
     EnvironmentModule,
     HealthzModule,
     InfrastructureModule,
+    KeycloakModule,
     LogModule,
     PluginModule,
     ProjectBulkModule,
     ProjectHooksModule,
     ProjectMembersModule,
-    ProjectModule,
     ProjectRolesModule,
     ProjectSecretsModule,
     ProjectServicesModule,
     RepositoryModule,
+    ProjectModule,
+    ServiceChainModule,
+    SystemSettingsModule,
     ScheduleModule.forRoot(),
     SystemConfigModule,
-    SystemSettingsModule,
+    UserTokensModule,
     VersionModule,
   ],
   controllers: [],
