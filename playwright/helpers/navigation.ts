ad95b2cdd19201d8595265e73c8e51670abae1a1
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2026-06-29 08:52:04 +0200

    chore(playwright): improve tests resilience

--- playwright/helpers/navigation.ts
+++ playwright/helpers/navigation.ts
@@ -1,9 +1,7 @@
 import type { Page } from '@playwright/test'
-import { expect } from '@playwright/test'
 
 export async function openMyProjects({ page }: { page: Page }) {
   await page.getByTestId('menuMyProjects').click()
-  await expect(page.getByTestId('cpin-loader')).toHaveCount(0)
 }
 
 async function ensureAdminMenuVisible(page: Page, menuTestId: string) {
@@ -15,17 +13,14 @@ async function ensureAdminMenuVisible(page: Page, menuTestId: string) {
 export async function openClustersAdministration({ page }: { page: Page }) {
   await ensureAdminMenuVisible(page, 'menuAdministrationClusters')
   await page.getByTestId('menuAdministrationClusters').click()
-  await expect(page.getByTestId('cpin-loader')).toHaveCount(0)
 }
 
 export async function openStagesAdministration({ page }: { page: Page }) {
   await ensureAdminMenuVisible(page, 'menuAdministrationStages')
   await page.getByTestId('menuAdministrationStages').click()
-  await expect(page.getByTestId('cpin-loader')).toHaveCount(0)
 }
 
 export async function openZonesAdministration({ page }: { page: Page }) {
   await ensureAdminMenuVisible(page, 'menuAdministrationZones')
   await page.getByTestId('menuAdministrationZones').click()
-  await expect(page.getByTestId('cpin-loader')).toHaveCount(0)
 }
