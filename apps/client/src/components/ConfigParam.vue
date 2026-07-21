dac92f5b6a5224fd7932b5dad564c53f667cb97a
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-07-21 15:54:46 +0200

    feat: clarify default config value
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
    Change-Id: I5a17621ae565448691163d6a93081cb06a6a6964

--- apps/client/src/components/ConfigParam.vue
+++ apps/client/src/components/ConfigParam.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { DEFAULT, DISABLED, ENABLED } from '@cpn-console/shared'
+import { toValue } from 'vue'
 
 const props = defineProps<{
   options: {
@@ -9,6 +10,7 @@ const props = defineProps<{
     disabled: boolean
     kind: 'text' | 'switch'
     placeholder: string | undefined
+    initialValue?: string
   }
 }>()
 
@@ -30,7 +32,13 @@ const switchOptions = [
   },
 ]
 
-const value = ref(props.options.value)
+const value = ref(toValue(props.options.value))
+
+const switchLabelByValue: Record<string, string> = {
+  [ENABLED]: 'Activé',
+  [DISABLED]: 'Désactivé',
+  [DEFAULT]: 'Par défaut',
+}
 
 function set(data: string) {
   value.value = data
@@ -52,17 +60,21 @@ function set(data: string) {
     :disabled="props.options.disabled"
     @update:model-value="(event: string | number | undefined) => set(event as string)"
   />
-  <DsfrSegmentedSet
-    v-else-if="props.options.kind === 'switch'"
-    :name="options.name"
-    :model-value="value"
-    :legend="props.options.name"
-    :hint="props.options.description"
-    :options="switchOptions"
-    :disabled="props.options.disabled"
-    data-testid="switch"
-    @update:model-value="(event: string | number) => set(String(event))"
-  />
+  <div v-else-if="props.options.kind === 'switch'">
+    <DsfrSegmentedSet
+      :name="props.options.name"
+      :model-value="value"
+      :legend="props.options.name"
+      :hint="props.options.description"
+      :options="switchOptions"
+      :disabled="props.options.disabled"
+      data-testid="switch"
+      @update:model-value="(event: string | number) => set(String(event))"
+    />
+    <p v-if="props.options.initialValue" class="text-xs text-gray-600 mt-1">
+      Valeur par défaut backend : {{ switchLabelByValue[props.options.initialValue] ?? props.options.initialValue }}
+    </p>
+  </div>
   <hr
     class="col-span-2 p-1"
   >
