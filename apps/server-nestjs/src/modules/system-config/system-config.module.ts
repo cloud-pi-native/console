069ac46989c7cde709bb7f575c67e900a6ae5df2
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-08-27 14:03:29 +0200

    fix(server-nestjs): import AuthModule in SystemConfigModule to resolve UserGuard dep

--- apps/server-nestjs/src/modules/system-config/system-config.module.ts
+++ apps/server-nestjs/src/modules/system-config/system-config.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from '../infrastructure/database/database.module'
 import { UserPermissionModule } from '../infrastructure/permission/user/user.module'
+import { AuthModule } from '../infrastructure/auth/auth.module'
 import { SystemConfigController } from './system-config.controller'
 import { SystemConfigService } from './system-config.service'
 
 @Module({
-  imports: [DatabaseModule, UserPermissionModule],
+  imports: [DatabaseModule, UserPermissionModule, AuthModule],
   controllers: [SystemConfigController],
   providers: [SystemConfigService],
 })
