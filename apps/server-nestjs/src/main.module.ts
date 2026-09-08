2967d926ea63f9682c81479e490eb3850611853e
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-09-08 20:07:15 +0200

    fix: OpenCDS should have been enabled though plugin service not from main
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
    Change-Id: I379f48b69cfbac57e8e7b6d0cc78ca0f6a6a6964

--- apps/server-nestjs/src/main.module.ts
+++ apps/server-nestjs/src/main.module.ts
@@ -20,7 +20,6 @@ import { ProjectServicesModule } from './modules/project-services/project-servic
 import { ProjectModule } from './modules/project/project.module'
 import { RepositoryModule } from './modules/repository/repository.module'
 import { SystemConfigModule } from './modules/system-config/system-config.module'
-import { ServiceChainModule } from './modules/service-chain/service-chain.module'
 import { SystemSettingsModule } from './modules/system-settings/system-settings.module'
 import { VersionModule } from './modules/version/version.module'
 import { getDotenvPaths } from './utils/dotenv.utils'
@@ -50,7 +49,6 @@ import { getDotenvPaths } from './utils/dotenv.utils'
     RepositoryModule,
     ScheduleModule.forRoot(),
     SystemConfigModule,
-    ServiceChainModule,
     SystemSettingsModule,
     VersionModule,
   ],
