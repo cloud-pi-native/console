a53f143eff0e761a863551d2ac3af12a183172a9
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-09-18 11:49:14 +0200

    fix(server-nestjs): register AdminTokenModule for downstream stacks
    rebases cleanly.
    
    Signed-off-by: Shikanime Deva <22115108+shikanime@users.noreply.github.com>

--- apps/server-nestjs/src/main.module.ts
+++ apps/server-nestjs/src/main.module.ts
@@ -4,6 +4,7 @@ import { ScheduleModule } from '@nestjs/schedule'
 import { TerminusModule } from '@nestjs/terminus'
 import { baseConfigFactory } from './config/base.config'
 import { AdminRoleModule } from './modules/admin-role/admin-role.module'
+import { AdminTokenModule } from './modules/admin-token/admin-token.module'
 import { ClusterModule } from './modules/cluster/cluster.module'
 import { DeploymentModule } from './modules/deployment/deployment.module'
 import { EnvironmentModule } from './modules/environment/environment.module'
@@ -34,6 +35,7 @@ import { getDotenvPaths } from './utils/dotenv.utils'
     }),
     TerminusModule.forRoot(),
     AdminRoleModule,
+    AdminTokenModule,
     ClusterModule,
     DeploymentModule,
     EnvironmentModule,
